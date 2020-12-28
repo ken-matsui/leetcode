8d168c71bc65de159d2a0a2090d188cd0c8be840
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2020-12-28 23:30:17 +0900

    Time: 32 ms (73.97%), Space: 23.5 MB (77.97%) - LeetHub

---
+++ reverse-string/reverse-string.cpp
@@ -0,0 +1,15 @@
+static auto _ = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    return nullptr;
+}();
+​
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        for (int i = 0; i < s.size() / 2; ++i) {
+            swap(s[i], s[s.size()-1-i]); 
+        }
+    }
+};
+​
