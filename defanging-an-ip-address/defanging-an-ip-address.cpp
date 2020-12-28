d7474fcd96f88629ae4bad3b63e702d2e3fb9842
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2020-12-28 23:37:26 +0900

    Time: 0 ms (100.00%), Space: 6.1 MB (85.93%) - LeetHub

---
+++ defanging-an-ip-address/defanging-an-ip-address.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    string defangIPaddr(string address) {
+        const auto first = address.begin();
+        string::size_type itr = 0;
+        for (int i = 0; i < 3; ++i) {
+            itr = address.find(".", itr);
+            address.replace(itr, 1, "[.]");
+            itr += 2;
+        }
+        return address;
+    }
+};
+​
