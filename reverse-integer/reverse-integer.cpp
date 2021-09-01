5e291b8255f8e9f05be2c55d423647485b0d7001
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2021-09-02 02:21:35 +0900

    Time: 5 ms (40.45%), Space: 6.4 MB (6.90%) - LeetHub

--- reverse-integer/reverse-integer.cpp
+++ reverse-integer/reverse-integer.cpp
@@ -1,16 +1,13 @@
-#include <cmath>
-#include <string>
-
 class Solution {
 public:
     int reverse(int x) {
         try {
             const bool is_negative = x < 0;
         
-            std::string str = std::to_string(std::abs(x));
+            string str = to_string(abs(x));
             std::reverse(str.begin(), str.end());
         
-            const int val = std::stoi(str);
+            const int val = stoi(str);
             return is_negative ? val * -1 : val;
         } catch (...) {
             return 0;
