ecadd246ab394e24e7e5c2020632113d207e45ad
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2021-09-02 17:34:50 +0900

    Time: 19 ms (19.11%), Space: 18.2 MB (93.40%) - LeetHub

--- remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
+++ remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
@@ -2,6 +2,6 @@ class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
         auto parsed = std::unique(nums.begin(), nums.end());
-        return nums.size() - std::distance(parsed, nums.end());        
+        return std::distance(nums.begin(), parsed);        
     }
 };
