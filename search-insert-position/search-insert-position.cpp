9070e82fc8bb81756d8933f93ff5b5a8173465a4
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2021-09-02 17:56:30 +0900

    Time: 0 ms (100.00%), Space: 9.7 MB (54.95%) - LeetHub

---
+++ search-insert-position/search-insert-position.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        auto found = std::find_if(
+            nums.begin(),
+            nums.end(),
+            [&target](int x){ return x >= target; }
+        );
+        return std::distance(nums.begin(), found);
+    }
+};
