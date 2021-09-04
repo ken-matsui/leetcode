2a59a2b667c587b6aad0ccad0f0ee532e95338e1
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2021-09-04 13:09:58 +0900

    Time: 11 ms (50.14%), Space: 12.7 MB (57.73%) - LeetHub

--- concatenation-of-array/concatenation-of-array.cpp
+++ concatenation-of-array/concatenation-of-array.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     vector<int> getConcatenation(vector<int>& nums) {
-        vector<int> ans(nums.begin(), nums.end());
+        vector<int> ans(nums);
         std::copy(nums.begin(), nums.end(), std::back_inserter(ans));
         return ans;
     }
