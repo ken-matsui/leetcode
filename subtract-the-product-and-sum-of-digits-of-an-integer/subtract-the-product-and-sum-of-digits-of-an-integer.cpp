aa82974e6dd832f46531b690a52305dc134e118a
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2021-09-04 21:54:23 +0900

    Time: 5 ms (6.28%), Space: 6 MB (28.28%) - LeetHub

---
+++ subtract-the-product-and-sum-of-digits-of-an-integer/subtract-the-product-and-sum-of-digits-of-an-integer.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int subtractProductAndSum(int n) {
+        string s = std::to_string(n);
+        
+        int product = 1;
+        int sum = 0;
+        for (const char c : s) {
+            const int x = c - '0';
+            product *= x;
+            sum += x;
+        }
+        return product - sum;
+    }
+};
