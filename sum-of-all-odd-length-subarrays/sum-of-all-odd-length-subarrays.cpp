71adc843a19c4d7328e610e27308dfad70a059c7
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2021-09-08 01:33:58 +0900

    Update sum-of-all-odd-length-subarrays.cpp

--- sum-of-all-odd-length-subarrays/sum-of-all-odd-length-subarrays.cpp
+++ sum-of-all-odd-length-subarrays/sum-of-all-odd-length-subarrays.cpp
@@ -2,15 +2,12 @@ class Solution {
 public:
     int sumOddLengthSubarrays(vector<int>& arr) {
         const size_t size = arr.size();
-        
-        int odd = 1;
+
         int sum = 0;
         for (int odd = 1; odd <= size; odd += 2) {
-            for (int i = 0; i < size; ++i) {
-                if (i + odd <= size) {
-                    for (int j = i; j < i + odd; ++j) {
-                        sum += arr[j];
-                    }
+            for (int i = 0; i + odd <= size; ++i) {
+                for (int j = i; j < i + odd; ++j) {
+                    sum += arr[j];
                 }
             }
         }
