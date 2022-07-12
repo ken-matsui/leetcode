823e00504d5570bdb8e36d646f09966170eceaa7
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2022-07-13 04:17:19 +0900

    Time: 558 ms (93.71%), Space: 0B (100.00%) - LeetHub

--- 1211-queries-quality-and-percentage/1211-queries-quality-and-percentage.sql
+++ 1211-queries-quality-and-percentage/1211-queries-quality-and-percentage.sql
@@ -1,17 +1,6 @@
 select
     query_name,
     round(avg(rating / position), 2) as quality,
-    round(
-        (
-            (
-                select count(*)
-                from Queries q2
-                where q1.query_name = q2.query_name
-                  and rating < 3
-            )  
-            /
-            count(*)
-        ) * 100
-    , 2) as poor_query_percentage
+    round((count(case when rating < 3 then 1 end) / count(*)) * 100, 2) as poor_query_percentage
 from Queries q1
 group by query_name;
