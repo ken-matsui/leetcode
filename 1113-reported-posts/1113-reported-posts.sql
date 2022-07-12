283d1f564fc8749638587c13f8b5363b57110328
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2022-07-13 02:34:42 +0900

    Time: 944 ms (17.78%), Space: 0B (100.00%) - LeetHub

---
+++ 1113-reported-posts/1113-reported-posts.sql
@@ -0,0 +1,8 @@
+select
+    extra as report_reason,
+    count(distinct(post_id)) as report_count
+from Actions
+where action_date = '2019/07/04'
+  and action = 'report'
+  and extra is not null
+group by extra;
