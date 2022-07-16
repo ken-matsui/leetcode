e1918e9ca156753b19cf05582cc366e80aa6bb86
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2022-07-17 07:08:51 +0900

    Time: 832 ms (67.68%), Space: 0B (100.00%) - LeetHub

--- 1607-sellers-with-no-sales/1607-sellers-with-no-sales.sql
+++ 1607-sellers-with-no-sales/1607-sellers-with-no-sales.sql
@@ -1,12 +1,7 @@
-with cte as (
-    select seller_id, order_cost
-    from Orders
-    where year(sale_date) = 2020
-)
-
 select seller_name
 from Seller
-left join cte
+left join Orders
 using (seller_id)
-where order_cost is null
+group by seller_id
+having count(case when year(sale_date) = 2020 then 1 end) = 0
 order by seller_name;
