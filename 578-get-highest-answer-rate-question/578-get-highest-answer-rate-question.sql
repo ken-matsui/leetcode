d1ccaa3d05b8f94c96940821099827dd274e1347
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2022-07-18 15:09:37 +0900

    Time: 489 ms (17.05%), Space: 0B (100.00%) - LeetHub

--- 578-get-highest-answer-rate-question/578-get-highest-answer-rate-question.sql
+++ 578-get-highest-answer-rate-question/578-get-highest-answer-rate-question.sql
@@ -1,15 +1,9 @@
-with cte as (
-    select
-        question_id,
-        count(case when action = 'answer' then 1 end)
-        /
-        count(case when action = 'show' then 1 end)
-        as answer_rate
-    from SurveyLog
-    group by question_id
-    order by answer_rate desc, question_id
-)
-
 select question_id as survey_log
-from cte
+from SurveyLog
+group by question_id
+order by
+    (count(case when action = 'answer' then 1 end)
+    /
+    count(case when action = 'show' then 1 end)) desc,
+    question_id
 limit 1;
