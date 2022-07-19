40af33f82810df256f6c8d6196eb02458978e429
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2022-07-19 09:11:32 +0900

    Time: 1714 ms (19.92%), Space: 0B (100.00%) - LeetHub

---
+++ 1070-product-sales-analysis-iii/1070-product-sales-analysis-iii.sql
@@ -0,0 +1,16 @@
+with cte as (
+    select
+        product_id,
+        year as first_year,
+        quantity,
+        price,
+        rank() over(
+            partition by product_id
+            order by year
+        ) as rnk
+    from Sales
+)
+
+select product_id, first_year, quantity, price
+from cte
+where rnk = 1;
