cb8826d4d5e4f09a389e9200b75b074041911987
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2022-09-14 06:36:19 -0700

    Time: 169 ms (64.27%), Space: 16.4 MB (39.02%) - LeetHub

---
+++ 1828-queries-on-number-of-points-inside-a-circle/1828-queries-on-number-of-points-inside-a-circle.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int distance(const int px, const int cx, const int py, const int cy) const {
+        return ceil(sqrt(pow(px - cx, 2) + pow(py - cy, 2)));
+    }
+
+    vector<int> countPoints(vector<vector<int>>& points, vector<vector<int>>& queries) {
+        vector<int> output;
+        for (const auto& q : queries) {
+            const int cx = q[0];
+            const int cy = q[1];
+            const int cr = q[2];
+
+            int count = 0;
+            for (const auto& p : points) {
+                const int px = p[0];
+                const int py = p[1];
+
+                int dist = distance(px, cx, py, cy);
+                if (dist <= cr) {
+                    ++count;
+                }
+            }
+            output.emplace_back(count);
+        }
+        return output;
+    }
+};
