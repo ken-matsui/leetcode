b7b3125124af54bedb7a2bff363e7b111cd1a884
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2022-09-14 00:26:28 -0700

    Time: 0 ms (100.00%), Space: 6.1 MB (81.54%) - LeetHub

---
+++ 709-to-lower-case/709-to-lower-case.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    string toLowerCase(string s) {
+        string out;
+        transform(s.cbegin(), s.cend(),  back_inserter(out), ::tolower);
+        return out;
+    }
+};
