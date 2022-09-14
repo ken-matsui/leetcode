546ebd204830a8167e1e30971aa79b92c0e261ba
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2022-09-13 23:55:08 -0700

    Time: 8 ms (61.01%), Space: 10.1 MB (8.09%) - LeetHub

---
+++ 1313-decompress-run-length-encoded-list/1313-decompress-run-length-encoded-list.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> decompressRLElist(vector<int>& nums) {
+        vector<int> ans;
+        
+        const size_t size = nums.size();
+        for (int i = 0; i < size; i += 2) {
+            const int freq = nums[i];
+            const int val = nums[i + 1];
+            
+            for (int j = 0; j < freq; ++j) {
+                ans.push_back(val);
+            }
+        }
+        
+        return ans;
+    }
+};
