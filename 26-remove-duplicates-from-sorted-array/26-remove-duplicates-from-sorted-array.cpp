6fa35d8565f28d0ad07792ed1789354348fe0a14
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2022-09-14 00:09:59 -0700

    Time: 16 ms (71.23%), Space: 18.5 MB (36.58%) - LeetHub

---
+++ 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.cpp
@@ -0,0 +1,7 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        auto parsed = std::unique(nums.begin(), nums.end());
+        return std::distance(nums.begin(), parsed);
+    }
+};
