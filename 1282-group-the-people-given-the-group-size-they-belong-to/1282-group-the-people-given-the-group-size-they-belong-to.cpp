85e0ed7699d6a74587511bb48110494a5a04f8d2
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2022-09-16 06:26:07 -0700

    Time: 27 ms (35.62%), Space: 12.4 MB (99.37%) - LeetHub

---
+++ 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<vector<int>> groupThePeople(vector<int>& groupSizes) {
+        vector<vector<int>> output;
+        unordered_map<int, vector<int>> cache;
+
+        const size_t size = groupSizes.size();
+        for (int i = 0; i < size; ++i) {
+            cache[groupSizes[i]].emplace_back(i);
+            
+            if (cache[groupSizes[i]].size() == groupSizes[i]) {
+                output.emplace_back(cache[groupSizes[i]]);
+                cache[groupSizes[i]] = {};
+            }
+        }
+
+        return output;
+    }
+};
