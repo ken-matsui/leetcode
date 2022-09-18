6822ee65a01dec5109bf1defcc11390fba611bda
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2022-09-18 13:33:43 -0700

    Time: 20 ms (38.16%), Space: 10 MB (5.14%) - LeetHub

---
+++ 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<vector<int>> diagonalSort(vector<vector<int>>& mat) {
+        const size_t colSize = mat.size();
+        const size_t rowSize = mat[0].size();
+        
+        unordered_map<int, deque<int>> diag;
+        
+        for (int i = 0; i < colSize; ++i) {
+            for (int j = 0; j < rowSize; ++j) {
+                diag[i - j].emplace_back(mat[i][j]);
+            }
+        }
+
+        for (auto& [key, val] : diag) {
+            sort(val.begin(), val.end());
+        }
+
+        for (int i = 0; i < colSize; ++i) {
+            for (int j = 0; j < rowSize; ++j) {
+                mat[i][j] = diag[i - j][0];
+                diag[i - j].pop_front();
+            }
+        }
+
+        return mat;
+    }
+};
