2f782931d426f0345ac9596643188ff5562b86d8
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2022-09-18 13:45:20 -0700

    Time: 159 ms (5.56%), Space: 176.2 MB (17.59%) - LeetHub

---
+++ 1490-clone-n-ary-tree/1490-clone-n-ary-tree.cpp
@@ -0,0 +1,34 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+public:
+    Node* cloneTree(Node* root) {
+        if (!root) {
+            return nullptr;
+        }
+
+        vector<Node*> children;
+        for (Node* n : root->children) {
+            children.emplace_back(cloneTree(n));
+        }
+        return new Node(root->val, children);
+    }
+};
