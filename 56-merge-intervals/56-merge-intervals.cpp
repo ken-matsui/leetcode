606c15c4cb5b2cff43636de2fa891e303c2e0a36
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2022-09-23 15:58:30 -0700

    Time: 86 ms (24.90%), Space: 19.7 MB (36.62%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.cpp
+++ 56-merge-intervals/56-merge-intervals.cpp
@@ -1,24 +1,19 @@
 class Solution {
-    size_t isize;
-
 public:
-    vector<int> merged(const vector<vector<int>>& intervals, int& i, vector<int>& res) {
-        if (i + 1 < isize && res[1] >= intervals[i + 1][0]) {
+    int findEnd(const vector<vector<int>>& intervals, int& i, const int prev) {
+        if (i + 1 < intervals.size() && prev >= intervals[i + 1][0]) {
             ++i;
-            res[0] = min(res[0], intervals[i][0]);
-            res[1] = max(res[1], intervals[i][1]);
-            return merged(intervals, i, res);
+            return findEnd(intervals, i, max(prev, intervals[i][1]));
         }
-        return res;
+        return prev;
     }
 
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        isize = intervals.size();
         sort(intervals.begin(), intervals.end(), [](auto& a, auto& b){ return a[0] < b[0]; });
 
         vector<vector<int>> output;
-        for (int i = 0; i < isize; ++i) {
-            output.push_back(merged(intervals, i, intervals[i]));
+        for (int i = 0; i < intervals.size(); ++i) {
+            output.push_back({intervals[i][0], findEnd(intervals, i, intervals[i][1])});
         }
         return output;
     }
