066baf7a8b6456af94b5b0bf4545c350b4d86307
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2022-09-23 16:00:13 -0700

    Time: 76 ms (38.78%), Space: 19.7 MB (37.32%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.cpp
+++ 56-merge-intervals/56-merge-intervals.cpp
@@ -1,7 +1,9 @@
 class Solution {
+    size_t isize;
+
 public:
     int findEnd(const vector<vector<int>>& intervals, int& i, const int prev) {
-        if (i + 1 < intervals.size() && prev >= intervals[i + 1][0]) {
+        if (i + 1 < isize && prev >= intervals[i + 1][0]) {
             ++i;
             return findEnd(intervals, i, max(prev, intervals[i][1]));
         }
@@ -11,8 +13,9 @@ public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
         sort(intervals.begin(), intervals.end(), [](auto& a, auto& b){ return a[0] < b[0]; });
 
+        isize = intervals.size();
         vector<vector<int>> output;
-        for (int i = 0; i < intervals.size(); ++i) {
+        for (int i = 0; i < isize; ++i) {
             output.push_back({intervals[i][0], findEnd(intervals, i, intervals[i][1])});
         }
         return output;
