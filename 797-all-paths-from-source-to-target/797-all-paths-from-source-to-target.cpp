f422fb79fbddfa8cb8f58ded64905a0f38e8cea4
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2022-09-23 07:09:04 -0700

    Time: 21 ms (65.72%), Space: 12 MB (51.38%) - LeetHub

---
+++ 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<vector<int>> ans;
+    vector<int> tmp;
+
+    void dfs(const vector<vector<int>>& graph, const int i){
+        if (i == graph.size() - 1) {
+            ans.push_back(tmp);
+            return;
+        }
+        for (const auto& x : graph[i]) {
+            tmp.push_back(x);
+            dfs(graph, x);
+            tmp.pop_back();
+        }     
+    }
+
+    vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
+        tmp.push_back(0);
+        dfs(graph, 0);
+        return ans;
+    }
+};
