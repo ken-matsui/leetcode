664a7ddd2353a77661aa269a4c6bc35b664f1488
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2022-09-24 12:30:15 -0700

    Time: 0 ms (100.00%), Space: 7.7 MB (78.27%) - LeetHub

--- 46-permutations/46-permutations.cpp
+++ 46-permutations/46-permutations.cpp
@@ -1,25 +1,13 @@
 class Solution {
-    size_t size;
-
 public:
-    void dfs(vector<int>& nums, int i, vector<vector<int>>& out) {
-		if (i == size) {
-		    out.emplace_back(nums);
-		    return;
-		}
-
-		for (int j = i; j < size; ++j) {
-		    swap(nums[i], nums[j]);
-		    dfs(nums, i + 1, out);
-		    swap(nums[i], nums[j]); // revert
-		}
-    }
-
     vector<vector<int>> permute(vector<int>& nums) {
-        size = nums.size();
+        const size_t size = nums.size();
+        sort(nums.begin(), nums.end());
 
         vector<vector<int>> out;
-	    dfs(nums, 0, out);
-	    return out;
+        do {
+            out.emplace_back(nums);
+        } while (next_permutation(nums.begin(), nums.end()));
+        return out;
     }
 };
