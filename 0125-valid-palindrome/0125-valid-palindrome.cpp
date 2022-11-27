9791f79fb0bf9a0b0beb15430293e6a1e2723506
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2022-11-27 06:19:45 -0800

    Time: 3 ms (97.53%), Space: 7.9 MB (19.35%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        string str;
+        for (char c : s) {
+            if (isalnum(c)) {
+                str += tolower(c);
+            }
+        }
+        
+        const int size = str.size();
+        const string left = str.substr(0, size / 2);
+        string right = str.substr(size / 2 + (size % 2));
+        reverse(right.begin(), right.end());
+        return left == right;
+    }
+};
