bef16c386b5720f5bec9c9598738de454315b4fc
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2022-11-30 07:14:29 -0800

    Time: 22 ms (83.84%), Space: 11.8 MB (22.96%) - LeetHub

---
+++ 0252-meeting-rooms/0252-meeting-rooms.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool canAttendMeetings(vector<vector<int>>& intervals) {
+        sort(intervals.begin(), intervals.end(), [](auto& l, auto& r){return l[0] < r[0];});
+        for (int i = 1; i < intervals.size(); i++) {
+            if (intervals[i][0] < intervals[i - 1][1]) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
