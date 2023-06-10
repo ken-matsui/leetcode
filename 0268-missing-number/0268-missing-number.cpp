4f5cf96abe145c46209be02a9bfac1d64f8ce016
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2023-06-10 02:08:54 -0700

    Time: 21 ms (64.10%), Space: 17.8 MB (99.98%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int size = nums.size();
+        
+        int expected = 0;
+        for (int i = 1; i <= size; ++i) {
+            expected += i;
+        }
+        
+        int actual = 0;
+        for (int n : nums) {
+            actual += n;
+        }
+        
+        return expected - actual;
+    }
+};
