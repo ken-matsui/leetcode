7d51c675bc2b14e0a4da34e4a6c5677fd4cf8ff6
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2023-08-29 00:20:15 -0700

    Time: 35 ms (65.20%), Space: 18.7 MB (14.68%) - LeetHub

---
+++ 0143-reorder-list/0143-reorder-list.cpp
@@ -0,0 +1,80 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+    void print(ListNode* node) {
+        cout << "[";
+        while (node) {
+            cout << node->val << ",";
+            node = node->next;
+        }
+        cout << "]" << endl;
+    }
+
+    ListNode* reverseList(ListNode* head) {
+        ListNode* prev = nullptr;
+        ListNode* curr = head;
+        while (curr) {
+            ListNode* nextTemp = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = nextTemp;
+        }
+        return prev;
+    }
+    
+    ListNode* middleNode(ListNode* head) {
+        int size = 0;
+        ListNode* tmp = head;
+        while (tmp) {
+            tmp = tmp->next;
+            ++size;
+        }
+        
+        for (int i = 0; i < size/2; ++i) {
+            head = head->next;
+        }
+        return head;
+    }
+
+public:
+    void reorderList(ListNode* head) {
+        if (!head->next || !head->next->next) {
+            return;
+        }
+        
+        ListNode* halfBack = reverseList(middleNode(head));
+        // print(halfBack);
+        // print(head);
+
+        // remove the last from halfBack
+        ListNode* tmp = halfBack;
+        ListNode* prev = tmp;
+        while (tmp->next) {
+            prev = tmp;
+            tmp = tmp->next;
+        }
+        prev->next = nullptr;
+        // print(halfBack);
+
+        while (head && halfBack) {
+            head->next = new ListNode(halfBack->val, head->next);
+
+            halfBack = halfBack->next;
+            if (head->next) {
+                head = head->next->next;
+                // cout << head->val;
+            } else {
+                head = nullptr;
+            }
+        }
+        // cout << "===" << endl;
+    }
+};
