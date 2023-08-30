4fdfd8571c30f329f08a6a81c66fde19783ecf0b
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2023-08-30 07:42:02 -0700

    Time: 0 ms (100.00%), Space: 6.9 MB (65.16%) - LeetHub

---
+++ 0054-spiral-matrix/0054-spiral-matrix.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+
+        vector<int> spiral;
+
+        int vert = 0;
+        int horiz = -1;
+
+        int direc = 1;
+        while (m*n > 0) {
+            // move horizontally
+            for (int i = 0; i < n; ++i) {
+                horiz += direc;
+                spiral.push_back(matrix[vert][horiz]);
+            }
+            --n; // update the boundary
+
+            // move vertically
+            --m; // update the boundary
+            for (int i = 0; i < m; ++i) {
+                vert += direc;
+                spiral.push_back(matrix[vert][horiz]);
+            }
+
+            direc *= -1; // flip
+        }
+
+        return spiral;
+    }
+};
