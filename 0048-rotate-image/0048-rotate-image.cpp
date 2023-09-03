d25697ad57e9cbe9c4f1c9928e1d584cfdccf2a1
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2023-09-03 03:19:59 -0700

    Time: 0 ms (100.00%), Space: 7 MB (54.71%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -1,15 +1,27 @@
 class Solution {
-public:
-    void rotate(vector<vector<int>>& matrix) {
-        int size = matrix.size();
-        for (int i = 0; i < size / 2 + size % 2; ++i) {
-            for (int j = 0; j < size / 2; ++j) {
-                int tmp = matrix[size - 1 - j][i];
-                matrix[size - 1 - j][i] = matrix[size - 1 - i][size - j - 1];
-                matrix[size - 1 - i][size - j - 1] = matrix[j][size - 1 - i];
-                matrix[j][size - 1 - i] = matrix[i][j];
+    int n;
+    
+    void trans(vector<vector<int>>& matrix) {
+        for (int i = 0; i < n; ++i) {
+            for (int j = i + 1; j < n; ++j) {
+                int tmp = matrix[j][i];
+                matrix[j][i] = matrix[i][j];
                 matrix[i][j] = tmp;
             }
         }
+    }
+
+    void rev(vector<vector<int>>& matrix) {
+        for (int i = 0; i < n; ++i) {
+            reverse(matrix[i].begin(), matrix[i].end());
+        }
+    }
+
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        n = matrix.size();
+
+        trans(matrix);
+        rev(matrix);
     }
 };
