6ba964c9563c4a43044db4fc7e3858cfd5345e55
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2023-09-03 15:44:20 -0700

    Time: 89 ms (53.69%), Space: 67.7 MB (86.16%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int maxSum = -10000000;
+        int curSum = 0;
+        for (int i = 0; i < nums.size(); ++i) {
+            curSum += nums[i];
+            if (curSum > maxSum) {
+                maxSum = curSum;
+            }
+            if (curSum <= 0) {
+                curSum = 0;
+            }
+        }
+        return maxSum;
+    }
+};
