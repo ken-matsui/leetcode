f0c62eabade46700ad16bf753a5250565bc4bd76
ken-matsui <ken-matsui@users.noreply.github.com>
2023-09-05 23:32:52 +0000

    Apply automatic changes

--- 0119-pascals-triangle-ii/0119-pascals-triangle-ii.cpp
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.cpp
@@ -1,9 +1,9 @@
 class Solution {
     vector<vector<int>> generate(int numRows) {
         vector<vector<int>> res(numRows);
-        
+
         for (int i = 0; i < numRows; ++i) {
-            res[i] = vector<int>(i+1, 1);
+            res[i] = vector<int>(i + 1, 1);
         }
         for (int i = 2; i < numRows; ++i) {
             for (int j = 1; j < i; ++j) {
@@ -13,9 +13,9 @@ class Solution {
 
         return res;
     }
-    
+
 public:
     vector<int> getRow(int rowIndex) {
-        return generate(rowIndex+1)[rowIndex];
+        return generate(rowIndex + 1)[rowIndex];
     }
 };
