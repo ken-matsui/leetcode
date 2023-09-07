645c23590a2f774a5937b3ae94b26376047d4b02
ken-matsui <ken-matsui@users.noreply.github.com>
2023-09-07 18:56:51 +0000

    Apply automatic changes

--- 0696-count-binary-substrings/0696-count-binary-substrings.cpp
+++ 0696-count-binary-substrings/0696-count-binary-substrings.cpp
@@ -5,17 +5,17 @@ public:
         groups[0] = 1;
         int t = 0;
         for (int i = 1; i < s.size(); ++i) {
-            if (s[i-1] != s[i]) {
+            if (s[i - 1] != s[i]) {
                 ++t;
                 groups[t] = 1;
             } else {
                 groups[t]++;
             }
         }
-        
+
         int ans = 0;
         for (int i = 1; i <= t; ++i) {
-            ans += min(groups[i-1], groups[i]);
+            ans += min(groups[i - 1], groups[i]);
         }
         return ans;
     }
