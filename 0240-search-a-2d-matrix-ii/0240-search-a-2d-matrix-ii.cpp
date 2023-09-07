f82b417935ef71020257e6c7f214bd55a296d142
ken-matsui <ken-matsui@users.noreply.github.com>
2023-09-07 17:37:43 +0000

    Apply automatic changes

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
@@ -3,9 +3,9 @@ public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         const int m = matrix.size();
         const int n = matrix[0].size();
-        
+
         // bottom-left
-        int i = m-1;
+        int i = m - 1;
         int j = 0;
 
         while (i >= 0 && j < n) {
