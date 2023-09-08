f77d7c531d448b875ff1bff2efc1cab372efe006
ken-matsui <ken-matsui@users.noreply.github.com>
2023-09-08 19:58:52 +0000

    Apply automatic changes

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -3,20 +3,20 @@ public:
     bool isValidSudoku(vector<vector<char>>& board) {
         // by row
         for (int i = 0; i < 9; ++i) {
-            
+
             unordered_set<char> set;
             for (int j = 0; j < 9; ++j) {
                 if (board[i][j] == '.') {
                     continue;
                 }
-                
+
                 auto [itr, did] = set.insert(board[i][j]);
                 if (!did) {
                     return false;
                 }
             }
         }
-        
+
         // by col
         for (int j = 0; j < 9; ++j) {
 
@@ -25,7 +25,7 @@ public:
                 if (board[i][j] == '.') {
                     continue;
                 }
-                
+
                 auto [itr, did] = set.insert(board[i][j]);
                 if (!did) {
                     return false;
@@ -36,14 +36,14 @@ public:
         // by 3x3
         for (int i_os = 0; i_os < 9; i_os += 3) {
             for (int j_os = 0; j_os < 9; j_os += 3) {
-                
+
                 unordered_set<char> set;
                 for (int i = i_os; i < i_os + 3; ++i) {
                     for (int j = j_os; j < j_os + 3; ++j) {
                         if (board[i][j] == '.') {
                             continue;
                         }
-                        
+
                         auto [itr, did] = set.insert(board[i][j]);
                         if (!did) {
                             return false;
