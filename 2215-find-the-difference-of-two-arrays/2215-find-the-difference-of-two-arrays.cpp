cb9384e93dbef4f4540e1af4e1d94eaa8a20ecab
ken-matsui <ken-matsui@users.noreply.github.com>
2023-09-17 13:19:49 +0000

    Apply automatic changes

--- 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.cpp
+++ 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.cpp
@@ -3,7 +3,7 @@ public:
     vector<vector<int>> findDifference(vector<int>& nums1, vector<int>& nums2) {
         unordered_set<int> one(nums1.begin(), nums1.end());
         unordered_set<int> two(nums2.begin(), nums2.end());
-        
+
         vector<vector<int>> res(2);
         for (const int n : one) {
             if (two.find(n) == two.end()) {
