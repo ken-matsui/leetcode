391bde6e520454fb8a15fac5ed7e3acf6cdbd829
ken-matsui <ken-matsui@users.noreply.github.com>
2023-09-17 08:07:13 +0000

    Apply automatic changes

--- 0412-fizz-buzz/0412-fizz-buzz.rs
+++ 0412-fizz-buzz/0412-fizz-buzz.rs
@@ -2,7 +2,7 @@ impl Solution {
     pub fn fizz_buzz(n: i32) -> Vec<String> {
         let mut v: Vec<String> = Vec::new();
 
-        for i in 1..n+1 {
+        for i in 1..n + 1 {
             let divisible_by_three: bool = i % 3 == 0;
             let divisible_by_five: bool = i % 5 == 0;
 
