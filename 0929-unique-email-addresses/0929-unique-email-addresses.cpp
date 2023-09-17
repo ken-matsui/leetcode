82fd127f6aa690cdfa3a7e59b95c1db1e4c783b8
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2023-09-17 06:12:45 -0700

    Time: 24 ms (48.04%), Space: 13.9 MB (64.41%) - LeetHub

---
+++ 0929-unique-email-addresses/0929-unique-email-addresses.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int numUniqueEmails(vector<string>& emails) {
+        unordered_set<string> set;
+        
+        for (const string& email : emails) {
+            string parsed;
+            bool plus = false;
+            bool at = false;
+            for (char c : email) {
+                if (c == '@') {
+                    parsed += c;
+                    at = true;
+                    continue;
+                }
+                if (at) {
+                    parsed += c;
+                    continue;
+                }
+                if (plus && c != '@') {
+                    continue;
+                }
+                if (c == '.') {
+                    continue;
+                }
+                if (c == '+') {
+                    plus = true;
+                    continue;
+                }
+                parsed += c;
+            }
+            set.insert(parsed);
+        }
+
+        return set.size();
+    }
+};
