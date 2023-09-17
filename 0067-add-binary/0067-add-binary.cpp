1a2efd952ef85f965f78040672abd5eb41eef94f
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2023-09-17 00:52:23 -0700

    Time: 0 ms (100.00%), Space: 6.9 MB (32.46%) - LeetHub

---
+++ 0067-add-binary/0067-add-binary.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    string addBinary(string a, string b) {
+        reverse(a.begin(), a.end());
+        reverse(b.begin(), b.end());
+        
+        string res;
+        
+        bool carry = false;
+        const int n = min(a.size(), b.size());
+        for (int i = 0; i < n; ++i) {
+            if (a[i] == '1' && b[i] == '1' && carry) {
+                res += '1';
+                carry = true;
+            } else if (a[i] == '1' && b[i] == '1' || a[i] == '1' && carry || b[i] == '1' && carry) {
+                res += '0';
+                carry = true;
+            } else if (a[i] == '1' || b[i] == '1' || carry) {
+                res += '1';
+                carry = false;
+            } else {
+                res += '0';
+                carry = false;
+            }
+        }
+
+        const int m = max(a.size(), b.size());
+        const string c = a.size() > b.size() ? a : b;
+        for (int i = n; i < m; ++i) {
+            if (c[i] == '1' && carry) {
+                res += '0';
+                carry = true;
+            } else if (c[i] == '1' || carry) {
+                res += '1';
+                carry = false;
+            } else {
+                res += '0';
+                carry = false;
+            }
+        }
+        if (carry) {
+            res += '1';
+        }
+
+        reverse(res.begin(), res.end());
+        return res;
+    }
+};
