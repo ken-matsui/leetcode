8e86d150d23278c462dbe126b9fe16dbff1231c5
ken-matsui <ken-matsui@users.noreply.github.com>
2023-09-17 13:07:20 +0000

    Apply automatic changes

--- 0027-remove-element/0027-remove-element.cpp
+++ 0027-remove-element/0027-remove-element.cpp
@@ -9,7 +9,7 @@ public:
             // everything was val
             return 0;
         }
-        
+
         for (int i = 0; i < right;) {
             if (nums[i] == val) {
                 swap(nums[i], nums[right]);
