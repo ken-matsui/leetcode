bd3399d15895d2012b4a3d85fea3e93343ea7511
ken-matsui <ken-matsui@users.noreply.github.com>
2023-09-17 06:09:30 +0000

    Apply automatic changes

--- 0621-task-scheduler/0621-task-scheduler.cpp
+++ 0621-task-scheduler/0621-task-scheduler.cpp
@@ -7,22 +7,22 @@ public:
         if (tasks.size() == 1) {
             return 1;
         }
-        
+
         unordered_map<char, int> freq;
         for (char task : tasks) {
             freq[task]++;
         }
-        
+
         priority_queue<int> maxHeap;
         for (auto itr : freq) {
             maxHeap.push(itr.second);
         }
-        
+
         int cycles = 0;
-    
+
         while (!maxHeap.empty()) {
             vector<int> tmp;
-            
+
             for (int i = 0; i < n + 1; ++i) {
                 if (!maxHeap.empty()) {
                     tmp.push_back(maxHeap.top());
