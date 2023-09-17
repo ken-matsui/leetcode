490469d95d9391a43929d001c46ba3c4a6df978d
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2023-09-17 01:06:56 -0700

    Format 0058-length-of-last-word

--- 0058-length-of-last-word/0058-length-of-last-word.rs
+++ 0058-length-of-last-word/0058-length-of-last-word.rs
@@ -1,5 +1,10 @@
 impl Solution {
     pub fn length_of_last_word(s: String) -> i32 {
-        s.split(' ').filter(|&x| !x.is_empty()).collect::<Vec<&str>>().last().unwrap().len() as i32
+        s.split(' ')
+            .filter(|&x| !x.is_empty())
+            .collect::<Vec<&str>>()
+            .last()
+            .unwrap()
+            .len() as i32
     }
 }
