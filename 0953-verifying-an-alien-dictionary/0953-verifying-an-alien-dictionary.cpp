5abaed2868f3b2ec24fa09952f90f355ff589e6d
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2023-09-17 01:52:51 -0700

    Time: 3 ms (82.52%), Space: 9.8 MB (39.55%) - LeetHub

---
+++ 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool isAlienSorted(vector<string>& words, string order) {
+        vector<int> orderMap(26);
+        for (int i = 0; i < order.size(); ++i) {
+            orderMap[order[i] - 'a'] = i;
+        }
+        
+        for (int i = 0; i < words.size() - 1; ++i) {
+            for (int j = 0; j < words[i].size(); ++j) {
+                if (j >= words[i + 1].size()) {
+                    return false;
+                }
+                
+                if (words[i][j] != words[i + 1][j]) {
+                    int curChar = words[i][j] - 'a';
+                    int nextChar = words[i + 1][j] - 'a';
+                    if (orderMap[curChar] > orderMap[nextChar]) {
+                        return false;
+                    } else {
+                        break;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+};
