34ce679aba6139eaab1f2aba007a66d04c3bac05
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2023-09-17 06:05:41 -0700

    Time: 2 ms (45.59%), Space: 9.4 MB (7.19%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int right = nums.size() - 1;
+        while (right >= 0 && nums[right] == val) {
+            --right;
+        }
+        if (right < 0) {
+            // everything was val
+            return 0;
+        }
+        
+        for (int i = 0; i < right;) {
+            if (nums[i] == val) {
+                swap(nums[i], nums[right]);
+                while (right > i && nums[right] == val) {
+                    --right;
+                }
+            } else {
+                ++i;
+            }
+        }
+
+        return right + 1;
+    }
+};
