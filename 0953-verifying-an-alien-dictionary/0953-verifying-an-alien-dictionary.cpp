116aa0d1380a26f1a5f1efb4cfe6e81f73944999
ken-matsui <ken-matsui@users.noreply.github.com>
2023-09-17 08:54:24 +0000

    Apply automatic changes

--- 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.cpp
+++ 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.cpp
@@ -5,13 +5,13 @@ public:
         for (int i = 0; i < order.size(); ++i) {
             orderMap[order[i] - 'a'] = i;
         }
-        
+
         for (int i = 0; i < words.size() - 1; ++i) {
             for (int j = 0; j < words[i].size(); ++j) {
                 if (j >= words[i + 1].size()) {
                     return false;
                 }
-                
+
                 if (words[i][j] != words[i + 1][j]) {
                     int curChar = words[i][j] - 'a';
                     int nextChar = words[i + 1][j] - 'a';
