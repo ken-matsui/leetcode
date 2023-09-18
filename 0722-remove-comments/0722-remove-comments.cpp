a5be5bfe4c6ddba9824c40800adb341badf7cc9f
ken-matsui <ken-matsui@users.noreply.github.com>
2023-09-18 10:53:09 +0000

    Apply automatic changes

--- 0722-remove-comments/0722-remove-comments.cpp
+++ 0722-remove-comments/0722-remove-comments.cpp
@@ -2,21 +2,24 @@ class Solution {
 public:
     vector<string> removeComments(vector<string>& source) {
         bool inBlockComment = false;
-        
+
         vector<string> newSource;
         string cur;
         for (const string& s : source) {
             const int size = s.size();
-            
+
             for (int i = 0; i < size; ++i) {
-                if (!inBlockComment && i < size - 1 && s[i] == '/' && s[i + 1] == '/') {
+                if (!inBlockComment && i < size - 1 && s[i] == '/' &&
+                    s[i + 1] == '/') {
                     // ignore line comment
                     break;
-                } else if (!inBlockComment && i < size - 1 && s[i] == '/' && s[i + 1] == '*') {
+                } else if (!inBlockComment && i < size - 1 && s[i] == '/' &&
+                           s[i + 1] == '*') {
                     // start `/*`
                     inBlockComment = true;
                     ++i; // skip `*`
-                } else if (inBlockComment && i < size - 1 && s[i] == '*' && s[i + 1] == '/') {
+                } else if (inBlockComment && i < size - 1 && s[i] == '*' &&
+                           s[i + 1] == '/') {
                     // end `*/`
                     inBlockComment = false;
                     ++i; // skip `/`
