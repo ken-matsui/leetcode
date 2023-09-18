80f5eda2631a23c45c67cb4b33d59ffad423168e
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2023-09-18 04:17:51 -0700

    Time: 18 ms (59.10%), Space: 14.8 MB (50.94%) - LeetHub

---
+++ 0332-reconstruct-itinerary/0332-reconstruct-itinerary.cpp
@@ -0,0 +1,24 @@
+class Solution {
+    unordered_map<string, priority_queue<string, vector<string>, greater<string>>> graph;
+    vector<string> result;
+    
+    void visit(const string& airport) {
+        while (!graph[airport].empty()) {
+            const string next = graph[airport].top();
+            graph[airport].pop();
+            visit(next);
+        }
+        result.push_back(airport);
+    }
+
+public:
+    vector<string> findItinerary(vector<vector<string>>& tickets) {
+        for (const auto& ticket : tickets) {
+            graph[ticket[0]].push(ticket[1]);
+        }
+        
+        visit("JFK");
+        reverse(result.begin(), result.end());
+        return result;
+    }
+};
