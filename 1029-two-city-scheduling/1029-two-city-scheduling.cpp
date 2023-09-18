e3e48e44743acdb33c0c0a623416771f7183ab1d
ken-matsui <ken-matsui@users.noreply.github.com>
2023-09-18 05:31:20 +0000

    Apply automatic changes

--- 1029-two-city-scheduling/1029-two-city-scheduling.cpp
+++ 1029-two-city-scheduling/1029-two-city-scheduling.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int twoCitySchedCost(vector<vector<int>>& costs) {
         const int size = costs.size();
-        
+
         // cost diff -> original idx
         vector<pair<int, int>> costDiff(size);
         for (int i = 0; i < size; ++i) {
@@ -10,9 +10,8 @@ public:
         }
 
         // minimize costs
-        sort(costDiff.begin(), costDiff.end(), [](const auto& a, const auto& b){
-            return a.first < b.first;
-        });
+        sort(costDiff.begin(), costDiff.end(),
+             [](const auto& a, const auto& b) { return a.first < b.first; });
 
         int sum = 0;
         for (int i = 0; i < size / 2; ++i) {
