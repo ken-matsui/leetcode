c13cbf5a41ac3f6b9a56331a8fdb80c7281084bc
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2023-09-18 00:27:51 -0700

    Time: 12 ms (93.42%), Space: 13.8 MB (57.09%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<vector<int>> adjList(numCourses);
+        vector<int> inDegree(numCourses, 0);
+        for (auto& edge : prerequisites) {
+            adjList[edge[1]].push_back(edge[0]);
+            inDegree[edge[0]]++;
+        }
+        
+        queue<int> zeroInDegree;
+        for (int i = 0; i < numCourses; ++i) {
+            if (inDegree[i] == 0) {
+                zeroInDegree.push(i);
+            }
+        }
+        
+        // Kahn's algorithm
+        vector<int> order;
+        while (!zeroInDegree.empty()) {
+            int node = zeroInDegree.front();
+            zeroInDegree.pop();
+            order.push_back(node);
+            
+            for (int& neighbor : adjList[node]) {
+                inDegree[neighbor]--;
+                if (inDegree[neighbor] == 0) {
+                    zeroInDegree.push(neighbor);
+                }
+            }
+        }
+        
+        if (order.size() == numCourses) {
+            return order;
+        } else {
+            return {};
+        }
+    }
+};
