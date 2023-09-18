433860cdaf00d677cb93548bbbe39296f78654a4
ken-matsui <ken-matsui@users.noreply.github.com>
2023-09-18 12:58:01 +0000

    Apply automatic changes

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -1,10 +1,11 @@
 class Solution {
-    void dfs(string current, int openRem, int closeRem, vector<string>& result) {
+    void dfs(string current, int openRem, int closeRem,
+             vector<string>& result) {
         if (openRem == 0 && closeRem == 0) {
             result.push_back(current);
             return;
         }
-        
+
         if (openRem > 0) {
             dfs(current + "(", openRem - 1, closeRem, result);
         }
@@ -12,7 +13,7 @@ class Solution {
             dfs(current + ")", openRem, closeRem - 1, result);
         }
     }
-    
+
 public:
     vector<string> generateParenthesis(int n) {
         vector<string> result;
