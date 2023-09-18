cc314312af40957d758fb726713771158cea2152
ken-matsui <ken-matsui@users.noreply.github.com>
2023-09-18 12:50:26 +0000

    Apply automatic changes

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -6,13 +6,13 @@ public:
         while (i < n && isspace(s[i])) {
             ++i;
         }
-        
+
         bool isNegative = false;
         if (i < n && (s[i] == '+' || s[i] == '-')) {
             isNegative = s[i] == '-';
             ++i;
         }
-        
+
         long long res = 0;
         while (i < n && isdigit(s[i])) {
             res = res * 10 + (s[i] - '0');
