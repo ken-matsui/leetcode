b7c849714912a2d97e9657dbc5089c6ef56736b2
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2023-09-18 00:24:06 -0700

    Time: 17 ms (66.84%), Space: 13.6 MB (72.25%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<vector<int>> adjList(numCourses);
+        vector<int> inDegree(numCourses, 0);
+        for (auto& edge : prerequisites) {
+            adjList[edge[1]].push_back(edge[0]);
+            inDegree[edge[0]]++;
+        }
+        
+        queue<int> zeroInDegree;
+        for (int i = 0; i < numCourses; ++i) {
+            if (inDegree[i] == 0) {
+                zeroInDegree.push(i);
+            }
+        }
+        
+        // Kahn's algorithm
+        int count = 0;
+        while (!zeroInDegree.empty()) {
+            int node = zeroInDegree.front();
+            zeroInDegree.pop();
+            ++count;
+            
+            for (int& neighbor : adjList[node]) {
+                inDegree[neighbor]--;
+                if (inDegree[neighbor] == 0) {
+                    zeroInDegree.push(neighbor);
+                }
+            }
+        }
+
+        return count == numCourses;
+    }
+};
