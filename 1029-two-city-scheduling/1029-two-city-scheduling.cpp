44108b2c5e40180ec15f65f83265e910cdf98786
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2023-09-17 22:28:33 -0700

    Time: 3 ms (80.05%), Space: 8.3 MB (25.63%) - LeetHub

---
+++ 1029-two-city-scheduling/1029-two-city-scheduling.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int twoCitySchedCost(vector<vector<int>>& costs) {
+        const int size = costs.size();
+        
+        // cost diff -> original idx
+        vector<pair<int, int>> costDiff(size);
+        for (int i = 0; i < size; ++i) {
+            costDiff[i] = {costs[i][0] - costs[i][1], i};
+        }
+
+        // minimize costs
+        sort(costDiff.begin(), costDiff.end(), [](const auto& a, const auto& b){
+            return a.first < b.first;
+        });
+
+        int sum = 0;
+        for (int i = 0; i < size / 2; ++i) {
+            sum += costs[costDiff[i].second][0]; // choose A
+        }
+        for (int i = size / 2; i < size; ++i) {
+            sum += costs[costDiff[i].second][1]; // choose B
+        }
+        return sum;
+    }
+};
