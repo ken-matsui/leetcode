9489fe8fb94d969615bf364f8578982996355d4e
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2023-09-18 05:55:38 -0700

    Time: 4 ms (51.21%), Space: 15.1 MB (36.24%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -0,0 +1,22 @@
+class Solution {
+    void dfs(string current, int openRem, int closeRem, vector<string>& result) {
+        if (openRem == 0 && closeRem == 0) {
+            result.push_back(current);
+            return;
+        }
+        
+        if (openRem > 0) {
+            dfs(current + "(", openRem - 1, closeRem, result);
+        }
+        if (closeRem > openRem) {
+            dfs(current + ")", openRem, closeRem - 1, result);
+        }
+    }
+    
+public:
+    vector<string> generateParenthesis(int n) {
+        vector<string> result;
+        dfs("", n, n, result);
+        return result;
+    }
+};
