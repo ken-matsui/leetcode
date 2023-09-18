9d23b98a1f5a367d64abdb69ec0fec70bd98da53
ken-matsui <ken-matsui@users.noreply.github.com>
2023-09-18 09:01:27 +0000

    Apply automatic changes

--- 0227-basic-calculator-ii/0227-basic-calculator-ii.cpp
+++ 0227-basic-calculator-ii/0227-basic-calculator-ii.cpp
@@ -2,7 +2,7 @@ class Solution {
     // infix -> postfix
     string shuntingYard(const string& s) {
         string out;
-        
+
         stack<char> ops;
         char prev = ' '; // for unary minus
         for (int i = 0; i < s.size(); ++i) {
@@ -18,17 +18,19 @@ class Solution {
                 out += ' '; // delimiter
                 --i;
             } else if (c == '+' || c == '-') {
-                if (c == '-' && (i == 0 || prev == '+' || prev == '-' || prev == '*' || prev == '/')) {
+                if (c == '-' && (i == 0 || prev == '+' || prev == '-' ||
+                                 prev == '*' || prev == '/')) {
                     // handle unary minus
                     out += "0 "; // add zero before the unary minus
                 }
-                while (!ops.empty() && (ops.top() == '+' || ops.top() == '-' || ops.top() == '*' || ops.top() == '/')) {
+                while (!ops.empty() && (ops.top() == '+' || ops.top() == '-' ||
+                                        ops.top() == '*' || ops.top() == '/')) {
                     out += ops.top();
                     out += " "; // delimiter for postfix
                     ops.pop();
                 }
                 ops.push(c);
-            }  else if (c == '*' || c == '/') {
+            } else if (c == '*' || c == '/') {
                 while (!ops.empty() && (ops.top() == '*' || ops.top() == '/')) {
                     out += ops.top();
                     out += " "; // delimiter for postfix
@@ -46,13 +48,13 @@ class Solution {
 
         return out;
     }
-    
+
     int evaluatePostfix(const string& s) {
         stack<int> st;
 
         for (int i = 0; i < s.size(); ++i) {
             const char c = s[i];
-            
+
             if (isdigit(c)) {
                 string n;
                 while (i < s.size() && s[i] != ' ') {
@@ -61,28 +63,30 @@ class Solution {
                 }
                 st.push(stoi(n));
             } else if (c != ' ') {
-                int b = st.top(); st.pop();
-                int a = st.top(); st.pop();
+                int b = st.top();
+                st.pop();
+                int a = st.top();
+                st.pop();
 
                 switch (c) {
-                    case '+':
-                        st.push(a + b);
-                        break;
-                    case '-':
-                        st.push(a - b);
-                        break;
-                    case '*':
-                        st.push(a * b);
-                        break;
-                    case '/':
-                        st.push(a / b);
-                        break;
+                case '+':
+                    st.push(a + b);
+                    break;
+                case '-':
+                    st.push(a - b);
+                    break;
+                case '*':
+                    st.push(a * b);
+                    break;
+                case '/':
+                    st.push(a / b);
+                    break;
                 }
             }
         }
         return st.top();
     }
-    
+
 public:
     int calculate(string s) {
         const string postfix = shuntingYard(s);
