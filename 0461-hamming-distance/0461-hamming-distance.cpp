b6bc6bdfc90b21700b5ed4481a8cd003b97b5144
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2023-09-18 06:47:36 -0700

    Time: 3 ms (17.22%), Space: 6.4 MB (6.87%) - LeetHub

---
+++ 0461-hamming-distance/0461-hamming-distance.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int hammingDistance(int x, int y) {
+        int dist = 0;
+        while (x || y) {
+            if ((x & 1) != (y & 1)) {
+                ++dist;
+            }
+            x >>= 1;
+            y >>= 1;
+        }
+        return dist;
+    }
+};
