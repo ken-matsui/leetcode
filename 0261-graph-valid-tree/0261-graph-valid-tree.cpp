3f5fd76f3c5fa7b25e938e4a960d034f4b407170
ken-matsui <ken-matsui@users.noreply.github.com>
2023-09-18 06:23:58 +0000

    Apply automatic changes

--- 0261-graph-valid-tree/0261-graph-valid-tree.cpp
+++ 0261-graph-valid-tree/0261-graph-valid-tree.cpp
@@ -1,10 +1,11 @@
 class Solution {
-    bool hasCycle(int parent, int node, const vector<vector<int>>& adjList, vector<bool>& visited) {
+    bool hasCycle(int parent, int node, const vector<vector<int>>& adjList,
+                  vector<bool>& visited) {
         if (visited[node]) {
             return true;
         }
         visited[node] = true;
-        
+
         for (int neighbor : adjList[node]) {
             if (neighbor == parent) {
                 continue;
@@ -15,7 +16,7 @@ class Solution {
         }
         return false;
     }
-    
+
 public:
     bool validTree(int n, vector<vector<int>>& edges) {
         vector<vector<int>> adjList(n);
@@ -23,20 +24,20 @@ public:
             adjList[edge[0]].push_back(edge[1]);
             adjList[edge[1]].push_back(edge[0]);
         }
-        
+
         vector<bool> visited(n, false);
-        
+
         if (hasCycle(-1, 0, adjList, visited)) {
             return false;
         }
-        
+
         // every node is connected?
         for (bool v : visited) {
             if (!v) {
                 return false;
             }
         }
-        
+
         return true;
     }
 };
