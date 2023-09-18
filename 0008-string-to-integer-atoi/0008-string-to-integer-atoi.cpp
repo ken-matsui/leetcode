61e6941c7c63c3adf6e7d5d7141591b127c2a365
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2023-09-18 05:48:33 -0700

    Time: 3 ms (62.69%), Space: 6.5 MB (33.13%) - LeetHub

---
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int myAtoi(string s) {
+        const int n = s.size();
+        int i = 0;
+        while (i < n && isspace(s[i])) {
+            ++i;
+        }
+        
+        bool isNegative = false;
+        if (i < n && (s[i] == '+' || s[i] == '-')) {
+            isNegative = s[i] == '-';
+            ++i;
+        }
+        
+        long long res = 0;
+        while (i < n && isdigit(s[i])) {
+            res = res * 10 + (s[i] - '0');
+
+            if (!isNegative && res >= INT_MAX) {
+                return INT_MAX;
+            }
+            if (isNegative && -res <= INT_MIN) {
+                return INT_MIN;
+            }
+
+            ++i;
+        }
+        return isNegative ? -res : res;
+    }
+};
