59493732d7dbc52c04f7b0c75ef914cfceb3133b
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2023-09-20 04:34:26 -0700

    Time: 13 ms (39.15%), Space: 10.2 MB (58.76%) - LeetHub

---
+++ 0130-surrounded-regions/0130-surrounded-regions.cpp
@@ -0,0 +1,63 @@
+class Solution {
+    int m;
+    int n;
+    
+    void markSafe(vector<vector<char>>& board, const int i, const int j) {
+        board[i][j] = '/';
+        
+        // visit connections
+        // down
+        if (i + 1 < m && board[i + 1][j] == 'O') {
+            markSafe(board, i + 1, j);
+        }
+        // up
+        if (i - 1 >= 0 && board[i - 1][j] == 'O') {
+            markSafe(board, i - 1, j);
+        }
+        // right
+        if (j + 1 < n && board[i][j + 1] == 'O') {
+            markSafe(board, i, j + 1);
+        }
+        // left
+        if (j - 1 >= 0 && board[i][j - 1] == 'O') {
+            markSafe(board, i, j - 1);
+        }
+    }
+
+public:
+    void solve(vector<vector<char>>& board) {
+        m = board.size();
+        n = board[0].size();
+        
+        // visit boarders
+        // top & bottom
+        for (int j = 0; j < n; ++j) {
+            if (board[0][j] == 'O') {
+                markSafe(board, 0, j);
+            }
+            if (board[m-1][j] == 'O') {
+                markSafe(board, m-1, j);
+            }
+        }
+        // left & right
+        for (int i = 0; i < m; ++i) {
+            if (board[i][0] == 'O') {
+                markSafe(board, i, 0);
+            }
+            if (board[i][n-1] == 'O') {
+                markSafe(board, i, n-1);
+            }
+        }
+        
+        // process marks
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (board[i][j] == '/') {
+                    board[i][j] = 'O';
+                } else if (board[i][j] == 'O') {
+                    board[i][j] = 'X';
+                }
+            }
+        }
+    }
+};
