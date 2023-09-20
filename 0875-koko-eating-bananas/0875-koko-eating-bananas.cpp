0d143039425a9c3cf8501b33da04ec1549554db6
ken-matsui <ken-matsui@users.noreply.github.com>
2023-09-20 12:19:31 +0000

    Apply automatic changes

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -3,10 +3,10 @@ public:
     int minEatingSpeed(vector<int>& piles, int h) {
         int lo = 1;
         int hi = *max_element(piles.begin(), piles.end());
-        
+
         while (lo < hi) {
             int mi = lo + (hi - lo) / 2;
-            
+
             int totalHours = 0;
             for (int pile : piles) {
                 totalHours += (pile / mi) + (pile % mi ? 1 : 0);
