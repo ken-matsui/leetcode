a6de733252c28d34e8020dbc05fb78b0ed70ef8f
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2023-09-20 05:06:30 -0700

    Time: 37 ms (11.71%), Space: 28.1 MB (5.53%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int lo = 0;
+        int hi = nums.size()-1;
+        while (lo <= hi) {
+            int mi = lo + (hi - lo) / 2;
+            if (nums[mi] == target) {
+                return mi;
+            } else if (nums[mi] < target) {
+                lo = mi + 1;
+            } else {
+                hi = mi - 1;
+            }
+        }
+        return -1;
+    }
+};
