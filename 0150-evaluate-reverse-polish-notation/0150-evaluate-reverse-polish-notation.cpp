1ec25fb6cd608946ca2eeabd9c5befa8cd253838
ken-matsui <ken-matsui@users.noreply.github.com>
2023-09-20 15:30:39 +0000

    Apply automatic changes

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
@@ -2,29 +2,30 @@ class Solution {
 public:
     int evalRPN(vector<string>& tokens) {
         stack<int> st;
-        
+
         for (int i = 0; i < tokens.size(); ++i) {
-            if (tokens[i] != "+" && tokens[i] != "-" && tokens[i] != "*" && tokens[i] != "/") {
+            if (tokens[i] != "+" && tokens[i] != "-" && tokens[i] != "*" &&
+                tokens[i] != "/") {
                 st.push(stoi(tokens[i]));
             } else {
                 int b = st.top();
                 st.pop();
                 int a = st.top();
                 st.pop();
-                
+
                 switch (tokens[i][0]) {
-                    case '+':
-                        st.push(a + b);
-                        break;
-                    case '-':
-                        st.push(a - b);
-                        break;
-                    case '/':
-                        st.push(a / b);
-                        break;
-                    case '*':
-                        st.push(a * b);
-                        break;
+                case '+':
+                    st.push(a + b);
+                    break;
+                case '-':
+                    st.push(a - b);
+                    break;
+                case '/':
+                    st.push(a / b);
+                    break;
+                case '*':
+                    st.push(a * b);
+                    break;
                 }
             }
         }
