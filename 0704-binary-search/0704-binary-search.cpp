568b1b4bf9daa712e3b5713f45f3733dabd35de1
ken-matsui <ken-matsui@users.noreply.github.com>
2023-09-20 12:08:19 +0000

    Apply automatic changes

--- 0704-binary-search/0704-binary-search.cpp
+++ 0704-binary-search/0704-binary-search.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int search(vector<int>& nums, int target) {
         int lo = 0;
-        int hi = nums.size()-1;
+        int hi = nums.size() - 1;
         while (lo <= hi) {
             int mi = lo + (hi - lo) / 2;
             if (nums[mi] == target) {
