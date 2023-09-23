a46706c58239b1e5c6a81e8d2c8c3e0664f8d7af
ken-matsui <ken-matsui@users.noreply.github.com>
2023-09-23 05:07:18 +0000

    Apply automatic changes

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -4,15 +4,15 @@ public:
         if (n == 0) {
             return 1;
         }
-        
+
         double z = 1.;
         long long nn = n;
-        
+
         if (nn < 0) {
             x = 1 / x;
             nn = -nn;
         }
-        
+
         while (nn > 0) {
             if (nn % 2 == 1) {
                 z *= x;
