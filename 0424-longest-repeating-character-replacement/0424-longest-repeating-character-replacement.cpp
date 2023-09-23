c2de0e01cfaa926757b9a12ebd8d286d6471f319
ken-matsui <ken-matsui@users.noreply.github.com>
2023-09-23 09:13:00 +0000

    Apply automatic changes

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -6,7 +6,7 @@ class Solution {
 public:
     int characterReplacement(string s, int k) {
         unordered_set<char> chars(s.begin(), s.end());
-        
+
         int maxLen = 0;
         for (const char c : chars) {
             int start = 0;
@@ -15,7 +15,7 @@ public:
                 if (s[end] == c) {
                     ++count;
                 }
-                
+
                 while (!isWindowValid(start, end, count, k)) {
                     if (s[start] == c) {
                         --count;
