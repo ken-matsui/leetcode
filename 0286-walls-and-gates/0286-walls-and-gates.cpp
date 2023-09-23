732d75bca16330121781c5a2c2605d52408eb076
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2023-09-23 03:58:58 -0700

    Time: 29 ms (55.43%), Space: 14.7 MB (60.48%) - LeetHub

---
+++ 0286-walls-and-gates/0286-walls-and-gates.cpp
@@ -0,0 +1,43 @@
+class Solution {
+    const int inf = 2147483647;
+    
+    int m;
+    int n;
+    
+    void dfs(vector<vector<int>>& rooms, const int i, const int j, const int dist) {
+        if (i < 0 || i >= m || j < 0 || j >= n || rooms[i][j] == 0 || rooms[i][j] == -1 || rooms[i][j] <= dist) {
+            return;
+        }
+
+        rooms[i][j] = dist;
+        // bottom
+        dfs(rooms, i + 1, j, dist + 1);
+        // up
+        dfs(rooms, i - 1, j, dist + 1);
+        // right
+        dfs(rooms, i, j + 1, dist + 1);
+        // left
+        dfs(rooms, i, j - 1, dist + 1);
+    }
+    
+public:
+    void wallsAndGates(vector<vector<int>>& rooms) {
+        m = rooms.size();
+        n = rooms[0].size();
+
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (rooms[i][j] == 0) {
+                    // bottom
+                    dfs(rooms, i + 1, j, 1);
+                    // up
+                    dfs(rooms, i - 1, j, 1);
+                    // right
+                    dfs(rooms, i, j + 1, 1);
+                    // left
+                    dfs(rooms, i, j - 1, 1);
+                }
+            }
+        }
+    }
+};
