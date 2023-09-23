74aff13855ab899f7daf9ff9b980486b1701c6d3
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2023-09-22 21:50:48 -0700

    Time: 0 ms (100.00%), Space: 9 MB (13.28%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        if (digits.back() != 9) {
+            *(digits.end()-1) += 1;
+            return digits;
+        } else {
+            bool carry = true;
+            for (auto itr = digits.rbegin(); carry && itr != digits.rend(); ++itr) {
+                if (*itr == 9) {
+                    *itr = 0;
+                } else {
+                    *itr += 1;
+                    carry = false;
+                }
+            }
+            if (carry) {
+                vector<int> vec{1};
+                for (const int d : digits) {
+                    vec.push_back(d);
+                }
+                return vec;
+            }
+            return digits;
+        }
+    }
+};
