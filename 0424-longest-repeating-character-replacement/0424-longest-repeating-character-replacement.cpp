735084e296eb9f3db7d4deff3fdef4170be6bad2
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2023-09-23 02:11:10 -0700

    Time: 11 ms (52.71%), Space: 7.4 MB (10.18%) - LeetHub

---
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -0,0 +1,30 @@
+class Solution {
+    bool isWindowValid(int start, int end, int count, int k) {
+        return end + 1 - start - count <= k;
+    }
+
+public:
+    int characterReplacement(string s, int k) {
+        unordered_set<char> chars(s.begin(), s.end());
+        
+        int maxLen = 0;
+        for (const char c : chars) {
+            int start = 0;
+            int count = 0;
+            for (int end = 0; end < s.size(); ++end) {
+                if (s[end] == c) {
+                    ++count;
+                }
+                
+                while (!isWindowValid(start, end, count, k)) {
+                    if (s[start] == c) {
+                        --count;
+                    }
+                    ++start;
+                }
+                maxLen = max(maxLen, end + 1 - start);
+            }
+        }
+        return maxLen;
+    }
+};
