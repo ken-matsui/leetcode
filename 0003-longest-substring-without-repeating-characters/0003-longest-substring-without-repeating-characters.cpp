481e0f873d78018066a4bece3c7074e0346f5054
ken-matsui <ken-matsui@users.noreply.github.com>
2023-09-23 08:50:32 +0000

    Apply automatic changes

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -3,13 +3,13 @@ public:
     int lengthOfLongestSubstring(string s) {
         int left = 0;
         int right = 0;
-        
+
         unordered_map<char, int> chars;
         int res = 0;
         while (right < s.length()) {
             const char r = s[right];
             chars[r]++;
-            
+
             while (chars[r] > 1) {
                 const char l = s[left];
                 chars[l]--;
