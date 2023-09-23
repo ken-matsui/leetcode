f391327e99c1804c0c6a38c2a8af08f067c5cb23
ken-matsui <ken-matsui@users.noreply.github.com>
2023-09-23 04:52:36 +0000

    Apply automatic changes

--- 0066-plus-one/0066-plus-one.cpp
+++ 0066-plus-one/0066-plus-one.cpp
@@ -2,11 +2,12 @@ class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
         if (digits.back() != 9) {
-            *(digits.end()-1) += 1;
+            *(digits.end() - 1) += 1;
             return digits;
         } else {
             bool carry = true;
-            for (auto itr = digits.rbegin(); carry && itr != digits.rend(); ++itr) {
+            for (auto itr = digits.rbegin(); carry && itr != digits.rend();
+                 ++itr) {
                 if (*itr == 9) {
                     *itr = 0;
                 } else {
