ee738116a07b523a2bf07a6ca8a1e82404e4b18d
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2023-09-22 22:05:34 -0700

    Time: 0 ms (100.00%), Space: 6.3 MB (12.32%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        if (n == 0) {
+            return 1;
+        }
+        
+        double z = 1.;
+        long long nn = n;
+        
+        if (nn < 0) {
+            x = 1 / x;
+            nn = -nn;
+        }
+        
+        while (nn > 0) {
+            if (nn % 2 == 1) {
+                z *= x;
+            }
+            x *= x;
+            nn /= 2;
+        }
+        return z;
+    }
+};
