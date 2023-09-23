024586d37bc33309f5791c0912760ce35314b794
ken-matsui <ken-matsui@users.noreply.github.com>
2023-09-23 09:44:26 +0000

    Apply automatic changes

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -4,7 +4,7 @@ public:
         if (s1.size() > s2.size()) {
             return false;
         }
-        
+
         sort(s1.begin(), s1.end());
         for (int i = 0; i <= s2.size() - s1.size(); ++i) {
             string sub = s2.substr(i, s1.size());
