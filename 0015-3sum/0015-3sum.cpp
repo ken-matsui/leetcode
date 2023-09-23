070c3189ca0072fccf8fcc040b9ef7d7cfe5bcc8
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2023-09-23 01:16:25 -0700

    Time: 88 ms (91.72%), Space: 24.3 MB (66.32%) - LeetHub

---
+++ 0015-3sum/0015-3sum.cpp
@@ -0,0 +1,31 @@
+class Solution {
+    void twoSum(vector<int>& nums, int i, vector<vector<int>>& res) {
+        int lo = i + 1;
+        int hi = nums.size() - 1;
+        while (lo < hi) {
+            int sum = nums[i] + nums[lo] + nums[hi];
+            if (sum < 0) {
+                ++lo;
+            } else if (sum > 0) {
+                --hi;
+            } else {
+                res.push_back({nums[i], nums[lo++], nums[hi--]});
+                while (lo < hi && nums[lo] == nums[lo - 1]) {
+                    ++lo;
+                }
+            }
+        }
+    }
+
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> res;
+        for (int i = 0; i < nums.size() && nums[i] <= 0; ++i) {
+            if (i == 0 || nums[i - 1] != nums[i]) {
+                twoSum(nums, i, res);
+            }
+        }
+        return res;
+    }
+};
