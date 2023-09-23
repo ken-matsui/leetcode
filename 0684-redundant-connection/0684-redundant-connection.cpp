a1ae3feedea4b129dddbeaa6f1c8df780b8435d3
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2023-09-23 16:59:10 -0700

    Time: 16 ms (11.48%), Space: 13.1 MB (8.97%) - LeetHub

---
+++ 0684-redundant-connection/0684-redundant-connection.cpp
@@ -0,0 +1,32 @@
+class Solution {
+    bool dfs(unordered_map<int, vector<int>>& adjList, unordered_set<int>& visited, const int parent, const int node) {
+        if (visited.find(parent) != visited.end()) {
+            return false;
+        }
+        visited.insert(parent);
+        if (parent == node) {
+            return true;
+        }
+        
+        for (const int neighbor : adjList[parent]) {
+            if (dfs(adjList, visited, neighbor, node)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+public:
+    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
+        unordered_map<int, vector<int>> adjList;
+        for (const auto& edge : edges) {
+            unordered_set<int> visited;
+            if (!adjList[edge[0]].empty() && !adjList[edge[1]].empty() && dfs(adjList, visited, edge[0], edge[1])) {
+                return edge;
+            }
+            adjList[edge[0]].push_back(edge[1]);
+            adjList[edge[1]].push_back(edge[0]);
+        }
+        return {-1, -1};
+    }
+};
