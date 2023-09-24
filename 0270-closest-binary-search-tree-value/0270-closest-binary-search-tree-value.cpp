bf50bbbcefa4716faff2d2c72afe05631b19450c
ken-matsui <ken-matsui@users.noreply.github.com>
2023-09-24 02:57:54 +0000

    Apply automatic changes

--- 0270-closest-binary-search-tree-value/0270-closest-binary-search-tree-value.cpp
+++ 0270-closest-binary-search-tree-value/0270-closest-binary-search-tree-value.cpp
@@ -6,13 +6,14 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
     double minDiff = DBL_MAX;
     int closeVal = INT_MAX;
-    
+
 public:
     int closestValue(TreeNode* root, double target) {
         if (!root) {
