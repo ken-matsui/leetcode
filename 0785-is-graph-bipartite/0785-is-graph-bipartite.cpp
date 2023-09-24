1ef2ddbe52b831ac1e828d06df48784a55b67937
ken-matsui <ken-matsui@users.noreply.github.com>
2023-09-24 01:21:34 +0000

    Apply automatic changes

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
@@ -3,7 +3,7 @@ class Solution {
         if (color[node] == -1) {
             color[node] = 0;
         }
-        
+
         for (const int neighbor : graph[node]) {
             if (color[neighbor] == color[node]) {
                 return false;
@@ -17,12 +17,12 @@ class Solution {
         }
         return true;
     }
-    
+
 public:
     bool isBipartite(vector<vector<int>>& graph) {
         const int n = graph.size();
         vector<int> color(n, -1);
-        
+
         for (int i = 0; i < n; ++i) {
             if (color[i] != -1) {
                 continue;
