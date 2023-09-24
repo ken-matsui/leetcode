34eede02487f88d77f9e5f10fcfa6bd6a276139d
ken-matsui <ken-matsui@users.noreply.github.com>
2023-09-24 07:15:35 +0000

    Apply automatic changes

--- 0257-binary-tree-paths/0257-binary-tree-paths.cpp
+++ 0257-binary-tree-paths/0257-binary-tree-paths.cpp
@@ -6,26 +6,27 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
     vector<string> res;
-    
+
     void preorder(TreeNode* node, string path) {
         if (!node) {
             return;
         }
-        
+
         path += "->" + to_string(node->val);
         if (!node->left && !node->right) {
             res.push_back(path);
         }
-        
+
         preorder(node->left, path);
         preorder(node->right, path);
     }
-    
+
 public:
     vector<string> binaryTreePaths(TreeNode* root) {
         if (!root) {
