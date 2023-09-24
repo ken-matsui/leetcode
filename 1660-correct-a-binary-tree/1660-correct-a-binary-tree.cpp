8ef9cae0751aa9136a7d565f6625681f0d19d78a
ken-matsui <ken-matsui@users.noreply.github.com>
2023-09-24 03:56:39 +0000

    Apply automatic changes

--- 1660-correct-a-binary-tree/1660-correct-a-binary-tree.cpp
+++ 1660-correct-a-binary-tree/1660-correct-a-binary-tree.cpp
@@ -6,12 +6,13 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
     unordered_set<int> visited;
-    
+
 public:
     TreeNode* correctBinaryTree(TreeNode* root) {
         if (!root) {
