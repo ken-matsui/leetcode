bf6a4e6c40a029273aedfb66039c6ca38938e8d7
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2023-09-23 21:29:48 -0700

    Time: 5 ms (43.46%), Space: 7.6 MB (18.32%) - LeetHub

---
+++ 0430-flatten-a-multilevel-doubly-linked-list/0430-flatten-a-multilevel-doubly-linked-list.cpp
@@ -0,0 +1,37 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* prev;
+    Node* next;
+    Node* child;
+};
+*/
+
+class Solution {
+    Node* dfs(Node* prev, Node* cur) {
+        if (!cur) {
+            return prev;
+        }
+        cur->prev = prev;
+        prev->next = cur;
+        
+        Node* tmp = cur->next;
+        
+        Node* tail = dfs(cur, cur->child);
+        cur->child = nullptr;
+        return dfs(tail, tmp);
+    }
+    
+public:
+    Node* flatten(Node* head) {
+        if (!head) {
+            return nullptr;
+        }
+        Node* prev = new Node(0, nullptr, head, nullptr);
+        dfs(prev, head);
+        prev->next->prev = nullptr;
+        return prev->next;
+    }
+};
