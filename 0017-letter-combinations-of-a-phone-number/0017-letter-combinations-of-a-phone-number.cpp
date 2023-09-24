4a6da3fe35d02aa3a98f533da4c6822c5c7dabe3
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2023-09-24 02:53:38 -0700

    Time: 0 ms (100.00%), Space: 6.8 MB (12.67%) - LeetHub

---
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -0,0 +1,30 @@
+class Solution {
+    vector<string> letters = {
+        "", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"
+    };
+    vector<string> res;
+    
+    void backtrack(const string& digits, const int idx, string& cur) {
+        if (idx == digits.size()) {
+            res.push_back(cur);
+            return;
+        }
+
+        for (const char c : letters[digits[idx] - '0']) {
+            cur.push_back(c);
+            backtrack(digits, idx + 1, cur);
+            cur.pop_back();
+        }
+    }
+
+public:
+    vector<string> letterCombinations(string digits) {
+        if (digits.empty()) {
+            return {};
+        }
+        
+        string cur;
+        backtrack(digits, 0, cur);
+        return res;
+    }
+};
