1be2f2855f890b980fc44abe806a6feed0dc8ac6
ken-matsui <ken-matsui@users.noreply.github.com>
2023-09-24 09:55:23 +0000

    Apply automatic changes

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -1,9 +1,8 @@
 class Solution {
-    vector<string> letters = {
-        "", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"
-    };
+    vector<string> letters = {"",    "",    "abc",  "def", "ghi",
+                              "jkl", "mno", "pqrs", "tuv", "wxyz"};
     vector<string> res;
-    
+
     void backtrack(const string& digits, const int idx, string& cur) {
         if (idx == digits.size()) {
             res.push_back(cur);
@@ -22,7 +21,7 @@ public:
         if (digits.empty()) {
             return {};
         }
-        
+
         string cur;
         backtrack(digits, 0, cur);
         return res;
