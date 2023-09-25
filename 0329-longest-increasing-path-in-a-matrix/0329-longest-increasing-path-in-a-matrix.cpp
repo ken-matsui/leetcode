4ee1ec57ce8bec50c82129e54bbee836098123a9
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2023-09-25 04:51:24 -0700

    Time: 34 ms (73.62%), Space: 16.5 MB (36.96%) - LeetHub

---
+++ 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.cpp
@@ -0,0 +1,46 @@
+class Solution {
+    int m;
+    int n;
+    vector<vector<int>> dp;
+    
+    int dfs(const vector<vector<int>>& matrix, const int i, const int j) {
+        if (dp[i][j] != -1) {
+            return dp[i][j];
+        }
+        
+        int maxPath = 1;
+        // bottom
+        if (i + 1 < m && matrix[i + 1][j] > matrix[i][j]) {
+            maxPath = max(maxPath, dfs(matrix, i + 1, j) + 1);
+        }
+        // up
+        if (i - 1 >= 0 && matrix[i - 1][j] > matrix[i][j]) {
+            maxPath = max(maxPath, dfs(matrix, i - 1, j) + 1);
+        }
+        // right
+        if (j + 1 < n && matrix[i][j + 1] > matrix[i][j]) {
+            maxPath = max(maxPath, dfs(matrix, i, j + 1) + 1);
+        }
+        // left
+        if (j - 1 >= 0 && matrix[i][j - 1] > matrix[i][j]) {
+            maxPath = max(maxPath, dfs(matrix, i, j - 1) + 1);
+        }
+        dp[i][j] = maxPath;
+        return maxPath;
+    }
+
+public:
+    int longestIncreasingPath(vector<vector<int>>& matrix) {
+        m = matrix.size();
+        n = matrix[0].size();
+        dp = vector<vector<int>>(m, vector<int>(n, -1));
+
+        int maxPath = 0;
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                maxPath = max(maxPath, dfs(matrix, i, j));
+            }
+        }
+        return maxPath;
+    }
+};
