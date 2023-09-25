711e95f08825dbc7dc306681ad4f127755633860
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2023-09-25 12:58:47 -0700

    Time: 4 ms (20.04%), Space: 8.6 MB (6.52%) - LeetHub

---
+++ 1046-last-stone-weight/1046-last-stone-weight.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        vector<int> buckets(1001, 0);
+        for (const int stone : stones) {
+            buckets[stone]++;
+        }
+        
+        int biggest = 0;
+        int idx = 1000;
+        while (idx > 0) {
+            if (buckets[idx] == 0) {
+                --idx;
+                continue;
+            }
+            
+            if (biggest == 0) {
+                buckets[idx] %= 2;
+                if (buckets[idx] == 1) {
+                    biggest = idx;
+                }
+                --idx;
+            } else {
+                buckets[idx]--;
+                if (biggest - idx <= idx) {
+                    buckets[biggest - idx]++;
+                    biggest = 0;
+                } else {
+                    biggest -= idx;
+                }
+            }
+        }
+        return biggest;
+    }
+};
