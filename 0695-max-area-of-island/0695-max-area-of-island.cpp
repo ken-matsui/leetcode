acd80d9573dd5ae453aa2ad5c84cf2aab8d24719
ken-matsui <ken-matsui@users.noreply.github.com>
2023-09-25 23:55:50 +0000

    Apply automatic changes

--- 0695-max-area-of-island/0695-max-area-of-island.cpp
+++ 0695-max-area-of-island/0695-max-area-of-island.cpp
@@ -1,13 +1,13 @@
 class Solution {
     int m;
     int n;
-    
+
     void dfs(vector<vector<int>>& grid, const int i, const int j, int& count) {
         if (grid[i][j] == -1) {
             return;
         }
         grid[i][j] = -1; // mark as visited
-        
+
         // bottom
         if (i + 1 < m && grid[i + 1][j] == 1) {
             ++count;
@@ -29,12 +29,12 @@ class Solution {
             dfs(grid, i, j - 1, count);
         }
     }
-    
+
 public:
     int maxAreaOfIsland(vector<vector<int>>& grid) {
         m = grid.size();
         n = grid[0].size();
-        
+
         int maxArea = 0;
         for (int i = 0; i < m; ++i) {
             for (int j = 0; j < n; ++j) {
