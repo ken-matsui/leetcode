ea1fd6d371df61dfd7d3c97a4f0526318de01506
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2023-09-25 01:28:06 -0700

    Time: 0 ms (100.00%), Space: 6.6 MB (8.90%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,21 +1,18 @@
 class Solution {
-    int climbStairs(int i, int n, vector<int>& dp) {
-        if (i > n) {
-            return 0;
-        }
-        if (i == n) {
-            return 1;
-        }
-        if (dp[i] > 0) {
-            return dp[i];
-        }
-        dp[i] = climbStairs(i + 1, n, dp) + climbStairs(i + 2, n, dp);
-        return dp[i];
-    }
-
 public:
     int climbStairs(int n) {
+        if (n == 1) {
+            return 1;
+        } else if (n == 2) {
+            return 2;
+        }
+
         vector<int> dp(n + 1, 0);
-        return climbStairs(0, n, dp);
+        dp[1] = 1;
+        dp[2] = 2;
+        for (int i = 3; i < dp.size(); ++i) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp.back();
     }
 };
