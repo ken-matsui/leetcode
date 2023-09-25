980aa18367a035b99d4c546021d35e2aee2ae94e
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2023-09-25 02:48:21 -0700

    Time: 0 ms (100.00%), Space: 6.6 MB (6.10%) - LeetHub

---
+++ 0266-palindrome-permutation/0266-palindrome-permutation.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool canPermutePalindrome(string s) {
+        unordered_map<char, int> freq;
+        for (const char c : s) {
+            freq[c]++;
+        }
+        
+        int oddCount = 0;
+        for (const auto& [k, v] : freq) {
+            if (v % 2 != 0) {
+                ++oddCount;
+            }
+        }
+        return oddCount <= 1;
+    }
+};
