61fbc8ce1534f6e91146a094bc1fc7166518e8a4
ken-matsui <ken-matsui@users.noreply.github.com>
2023-09-25 22:02:08 +0000

    Apply automatic changes

--- 0886-possible-bipartition/0886-possible-bipartition.cpp
+++ 0886-possible-bipartition/0886-possible-bipartition.cpp
@@ -6,20 +6,20 @@ public:
             adjList[edge[0] - 1].push_back(edge[1] - 1);
             adjList[edge[1] - 1].push_back(edge[0] - 1);
         }
-        
+
         vector<int> colors(n, -1);
         for (int i = 0; i < n; ++i) {
             if (colors[i] != -1) {
                 continue;
             }
-            
+
             stack<int> st;
             st.push(i);
             colors[i] = 0;
             while (!st.empty()) {
                 const int node = st.top();
                 st.pop();
-                
+
                 for (const int neighbor : adjList[node]) {
                     if (colors[neighbor] == -1) {
                         colors[neighbor] = colors[node] ^ 1;
