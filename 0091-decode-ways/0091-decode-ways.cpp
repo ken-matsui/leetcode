db017c8c13111920406fe69ad2e9e3de16483b91
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2023-09-25 06:49:30 -0700

    Time: 2 ms (51.18%), Space: 7 MB (26.84%) - LeetHub

---
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -0,0 +1,30 @@
+class Solution {
+    unordered_map<int, int> dp;
+    
+    int traverse(int idx, const string& s) {
+        if (dp.find(idx) != dp.end()) {
+            return dp[idx];
+        }
+        if (idx == s.size()) {
+            return 1;
+        }
+        if (s[idx] == '0') {
+            return 0;
+        }
+        if (idx == s.size() - 1) {
+            return 1;
+        }
+        
+        int res = traverse(idx + 1, s);
+        if (stoi(s.substr(idx, 2)) <= 26) {
+            res += traverse(idx + 2, s);
+        }
+        dp[idx] = res;
+        return res;
+    }
+    
+public:
+    int numDecodings(string s) {
+        return traverse(0, s);
+    }
+};
