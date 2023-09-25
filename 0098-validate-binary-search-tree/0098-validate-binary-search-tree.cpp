ba827e9ee868286b547ec5bd3702d923e48ecbfd
ken-matsui <ken-matsui@users.noreply.github.com>
2023-09-25 23:29:31 +0000

    Apply automatic changes

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
@@ -6,7 +6,8 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
@@ -14,16 +15,17 @@ class Solution {
         if (!node) {
             return true;
         }
-        
+
         if (low && node->val <= low->val) {
             return false;
         }
         if (high && node->val >= high->val) {
             return false;
         }
-        return traverse(node->left, low, node) && traverse(node->right, node, high);
+        return traverse(node->left, low, node) &&
+               traverse(node->right, node, high);
     }
-    
+
 public:
     bool isValidBST(TreeNode* root) {
         return traverse(root, nullptr, nullptr);
