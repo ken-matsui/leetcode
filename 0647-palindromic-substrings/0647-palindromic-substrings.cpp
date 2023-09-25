8910239e239929519f311f789bb05cf0323916a1
ken-matsui <ken-matsui@users.noreply.github.com>
2023-09-25 09:40:07 +0000

    Apply automatic changes

--- 0647-palindromic-substrings/0647-palindromic-substrings.cpp
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -3,7 +3,7 @@ class Solution {
         int count = 0;
         while (left >= 0 && right < s.size() && s[left] == s[right]) {
             ++count; // Found a palindrome substring
-            --left; // Expand to the left
+            --left;  // Expand to the left
             ++right; // Expand to the right
         }
         return count;
@@ -12,8 +12,8 @@ class Solution {
 public:
     int countSubstrings(string s) {
         int count = 0;
-        for(int i = 0; i < s.size(); ++i) {
-            count += expandAroundCenter(s, i, i); // Odd length palindromes
+        for (int i = 0; i < s.size(); ++i) {
+            count += expandAroundCenter(s, i, i);     // Odd length palindromes
             count += expandAroundCenter(s, i, i + 1); // Even length palindromes
         }
         return count;
