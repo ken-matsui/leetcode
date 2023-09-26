f70e1fa6ffc3b60db7f1a8c87c824c11d4ca063a
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2023-09-26 11:02:38 -0700

    Time: 91 ms (91.66%), Space: 40.1 MB (91.99%) - LeetHub

---
+++ 1042-flower-planting-with-no-adjacent/1042-flower-planting-with-no-adjacent.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<int> gardenNoAdj(int n, vector<vector<int>>& paths) {
+        vector<vector<int>> adjList(n);
+        for (const auto& edge : paths) {
+            adjList[edge[0]-1].push_back(edge[1]-1);
+            adjList[edge[1]-1].push_back(edge[0]-1);
+        }
+        
+        vector<int> res(n, 0);
+        for (int i = 0; i < n; ++i) {
+            bool colors[5] = {}; // 4 colors
+            
+            for (const int neighbor : adjList[i]) {
+                colors[res[neighbor]] = true;
+            }
+
+            // Find unused colors
+            for (int c = 1; c <= 4; ++c) {
+                if (!colors[c]) {
+                    res[i] = c;
+                    break;
+                }
+            }
+        }
+        return res;
+    }
+};
