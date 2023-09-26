4edc68725b3f7febe73c9ac70bee8cf43feb1c08
ken-matsui <ken-matsui@users.noreply.github.com>
2023-09-26 01:37:44 +0000

    Apply automatic changes

--- 0130-surrounded-regions/0130-surrounded-regions.cpp
+++ 0130-surrounded-regions/0130-surrounded-regions.cpp
@@ -1,10 +1,10 @@
 class Solution {
     int m;
     int n;
-    
+
     void markSafe(vector<vector<char>>& board, const int i, const int j) {
         board[i][j] = '/'; // mark as safe
-        
+
         // bottom
         if (i + 1 < m && board[i + 1][j] == 'O') {
             markSafe(board, i + 1, j);
@@ -33,19 +33,19 @@ public:
             if (board[0][j] == 'O') {
                 markSafe(board, 0, j);
             }
-            if (board[m-1][j] == 'O') {
-                markSafe(board, m-1, j);
+            if (board[m - 1][j] == 'O') {
+                markSafe(board, m - 1, j);
             }
         }
         for (int i = 0; i < m; ++i) {
             if (board[i][0] == 'O') {
                 markSafe(board, i, 0);
             }
-            if (board[i][n-1] == 'O') {
-                markSafe(board, i, n-1);
+            if (board[i][n - 1] == 'O') {
+                markSafe(board, i, n - 1);
             }
         }
-        
+
         // convert all O's to X's
         for (int i = 0; i < m; ++i) {
             for (int j = 0; j < n; ++j) {
@@ -54,7 +54,7 @@ public:
                 }
             }
         }
-        
+
         // revert all /'s to O's
         for (int i = 0; i < m; ++i) {
             for (int j = 0; j < n; ++j) {
