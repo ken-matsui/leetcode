fd5d696280e5f1a4dc595dbc0abc8a75cac24a3c
ken-matsui <ken-matsui@users.noreply.github.com>
2023-09-26 18:04:11 +0000

    Apply automatic changes

--- 1042-flower-planting-with-no-adjacent/1042-flower-planting-with-no-adjacent.cpp
+++ 1042-flower-planting-with-no-adjacent/1042-flower-planting-with-no-adjacent.cpp
@@ -3,14 +3,14 @@ public:
     vector<int> gardenNoAdj(int n, vector<vector<int>>& paths) {
         vector<vector<int>> adjList(n);
         for (const auto& edge : paths) {
-            adjList[edge[0]-1].push_back(edge[1]-1);
-            adjList[edge[1]-1].push_back(edge[0]-1);
+            adjList[edge[0] - 1].push_back(edge[1] - 1);
+            adjList[edge[1] - 1].push_back(edge[0] - 1);
         }
-        
+
         vector<int> res(n, 0);
         for (int i = 0; i < n; ++i) {
             bool colors[5] = {}; // 4 colors
-            
+
             for (const int neighbor : adjList[i]) {
                 colors[res[neighbor]] = true;
             }
