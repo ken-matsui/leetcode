7558c7f45fc5295fdb9742ea0f6ef42a5cf39349
ken-matsui <ken-matsui@users.noreply.github.com>
2023-09-26 02:32:05 +0000

    Apply automatic changes

--- 1267-count-servers-that-communicate/1267-count-servers-that-communicate.cpp
+++ 1267-count-servers-that-communicate/1267-count-servers-that-communicate.cpp
@@ -3,10 +3,10 @@ public:
     int countServers(vector<vector<int>>& grid) {
         const int m = grid.size();
         const int n = grid[0].size();
-        
+
         vector<int> rowCount(m, 0);
         vector<int> colCount(n, 0);
-        
+
         for (int i = 0; i < m; ++i) {
             for (int j = 0; j < n; ++j) {
                 if (grid[i][j] == 1) {
@@ -15,7 +15,7 @@ public:
                 }
             }
         }
-        
+
         int count = 0;
         for (int i = 0; i < m; ++i) {
             for (int j = 0; j < n; ++j) {
