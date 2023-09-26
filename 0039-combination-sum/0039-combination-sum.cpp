2b8453d50a2c40183843ac1756ae3482f1d7d396
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2023-09-26 08:48:48 -0700

    Time: 4 ms (74.55%), Space: 11.1 MB (48.22%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -0,0 +1,26 @@
+class Solution {
+    vector<vector<int>> res;
+    
+    void backtrack(const vector<int>& candidates, const int target, vector<int>& cur, const int start) {
+        if (target == 0) {
+            res.push_back(cur);
+            return;
+        }
+        if (target < 0) {
+            return;
+        }
+        
+        for (int i = start; i < candidates.size(); ++i) {
+            cur.push_back(candidates[i]);
+            backtrack(candidates, target - candidates[i], cur, i);
+            cur.pop_back();
+        }
+    }
+    
+public:
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<int> cur;
+        backtrack(candidates, target, cur, 0);
+        return res;
+    }
+};
