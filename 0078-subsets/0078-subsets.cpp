79d3bf500486962821b804d290ef7b0c6e3c498b
ken-matsui <ken-matsui@users.noreply.github.com>
2023-09-26 15:42:07 +0000

    Apply automatic changes

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,9 +1,9 @@
 class Solution {
     vector<vector<int>> res;
-    
+
     void backtrack(const vector<int>& nums, vector<int>& cur, const int start) {
         res.push_back(cur);
-        
+
         for (int i = start; i < nums.size(); ++i) {
             cur.push_back(nums[i]);
             backtrack(nums, cur, i + 1);
