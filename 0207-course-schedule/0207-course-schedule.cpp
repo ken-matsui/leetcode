94091d222b6ca64b98a83c90e30e19330c34fc60
ken-matsui <ken-matsui@users.noreply.github.com>
2023-09-26 04:03:13 +0000

    Apply automatic changes

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -7,21 +7,21 @@ public:
             adjList[edge[1]].push_back(edge[0]);
             inDegree[edge[0]]++;
         }
-        
+
         queue<int> zeroInDegree;
         for (int i = 0; i < numCourses; ++i) {
             if (inDegree[i] == 0) {
                 zeroInDegree.push(i);
             }
         }
-        
+
         // Kahn's Algorithm
         int count = 0;
         while (!zeroInDegree.empty()) {
             const int node = zeroInDegree.front();
             zeroInDegree.pop();
             ++count;
-            
+
             for (const int neighbor : adjList[node]) {
                 inDegree[neighbor]--;
                 if (inDegree[neighbor] == 0) {
