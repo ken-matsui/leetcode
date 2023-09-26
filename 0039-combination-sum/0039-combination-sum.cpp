5e4412b386def11631c04798dea153794e002ac8
ken-matsui <ken-matsui@users.noreply.github.com>
2023-09-26 15:50:48 +0000

    Apply automatic changes

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,7 +1,8 @@
 class Solution {
     vector<vector<int>> res;
-    
-    void backtrack(const vector<int>& candidates, const int target, vector<int>& cur, const int start) {
+
+    void backtrack(const vector<int>& candidates, const int target,
+                   vector<int>& cur, const int start) {
         if (target == 0) {
             res.push_back(cur);
             return;
@@ -9,14 +10,14 @@ class Solution {
         if (target < 0) {
             return;
         }
-        
+
         for (int i = start; i < candidates.size(); ++i) {
             cur.push_back(candidates[i]);
             backtrack(candidates, target - candidates[i], cur, i);
             cur.pop_back();
         }
     }
-    
+
 public:
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
         vector<int> cur;
