9da946138d104421b69aaec9051d6dc1d136e838
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2024-05-03 20:49:06 -0700

    Time: 3 ms (42.00%), Space: 7.4 MB (85.84%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for (char c : s) {
+            switch (c) {
+                case ')':
+                    if (st.empty() || st.top() != '(')
+                        return false;
+                    st.pop();
+                    break;
+                case '}':
+                    if (st.empty() || st.top() != '{')
+                        return false;
+                    st.pop();
+                    break;
+                case ']':
+                    if (st.empty() || st.top() != '[')
+                        return false;
+                    st.pop();
+                    break;
+                default:
+                    st.push(c);
+                    break;
+            }
+        }
+        return st.empty();
+    }
+};
