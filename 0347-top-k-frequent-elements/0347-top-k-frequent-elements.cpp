029aae08951827406eab912305c6aa7efd7fd3f7
ken-matsui <ken-matsui@users.noreply.github.com>
2024-05-04 02:09:30 +0000

    Apply automatic changes

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -5,12 +5,12 @@ public:
         for (int n : nums) {
             m[n]++;
         }
-        
+
         priority_queue<pair<int, int>> pq;
         for (auto [v, freq] : m) {
             pq.push({freq, v});
         }
-        
+
         vector<int> res(k);
         for (int i = 0; i < k; ++i) {
             res[i] = pq.top().second;
