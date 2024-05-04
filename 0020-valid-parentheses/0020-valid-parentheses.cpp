7865e1b9ec0e2a532a6e8207570db5507f871ba4
ken-matsui <ken-matsui@users.noreply.github.com>
2024-05-04 03:50:35 +0000

    Apply automatic changes

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -4,24 +4,24 @@ public:
         stack<char> st;
         for (char c : s) {
             switch (c) {
-                case ')':
-                    if (st.empty() || st.top() != '(')
-                        return false;
-                    st.pop();
-                    break;
-                case '}':
-                    if (st.empty() || st.top() != '{')
-                        return false;
-                    st.pop();
-                    break;
-                case ']':
-                    if (st.empty() || st.top() != '[')
-                        return false;
-                    st.pop();
-                    break;
-                default:
-                    st.push(c);
-                    break;
+            case ')':
+                if (st.empty() || st.top() != '(')
+                    return false;
+                st.pop();
+                break;
+            case '}':
+                if (st.empty() || st.top() != '{')
+                    return false;
+                st.pop();
+                break;
+            case ']':
+                if (st.empty() || st.top() != '[')
+                    return false;
+                st.pop();
+                break;
+            default:
+                st.push(c);
+                break;
             }
         }
         return st.empty();
