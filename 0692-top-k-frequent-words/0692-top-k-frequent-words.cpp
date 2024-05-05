54f7bf739d5d4f21b14604ed537e07c0375bb715
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2024-05-05 10:44:35 -0700

    Time: 12 ms (48.73%), Space: 17.3 MB (50.34%) - LeetHub

---
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<string> topKFrequent(vector<string>& words, int k) {
+        unordered_map<string, int> freq;
+        for (const string& s : words) {
+            freq[s]++;
+        }
+
+        auto cmp = [](const pair<int, string>& left, const pair<int, string>& right) {
+            if (left.first != right.first) {
+                return left.first > right.first;
+            }
+            return left.second < right.second;
+        };
+        priority_queue<pair<int, string>, vector<pair<int, string>>, decltype(cmp)> pq(cmp);
+        for (const auto& [s, f] : freq) {
+            pq.push(make_pair(f, s));
+            if (pq.size() > k) {
+                pq.pop();
+            }
+        }
+
+        vector<string> res(k);
+        for (int i = k - 1; i >= 0; --i) {
+            res[i] = pq.top().second;
+            pq.pop();
+        }
+        return res;
+    }
+};
