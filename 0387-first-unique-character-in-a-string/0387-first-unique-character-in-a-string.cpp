3d11a398ace0367c0756350c0a4c806ea7ed3ef6
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2024-05-05 10:02:05 -0700

    Time: 24 ms (76.20%), Space: 12.8 MB (63.05%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.cpp
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     int firstUniqChar(string s) {
-        unordered_map<char, int> m;
+        vector<int> m(26);
         for (int i = 0; i < s.size(); ++i) {
-            m[s[i]]++;
+            m[s[i] - 'a']++;
         }
 
         for (int i = 0; i < s.size(); ++i) {
-            if (m[s[i]] == 1) {
+            if (m[s[i] - 'a'] == 1) {
                 return i;
             }
         }
