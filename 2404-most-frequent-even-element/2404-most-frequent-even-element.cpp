0c2f3ab1b13d0df5486f50af18c4b7527d17aeb8
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2024-05-05 09:40:59 -0700

    Time: 49 ms (84.79%), Space: 41.8 MB (40.23%) - LeetHub

---
+++ 2404-most-frequent-even-element/2404-most-frequent-even-element.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int mostFrequentEven(vector<int>& nums) {
+        unordered_map<int, int> freq;
+        for (int n : nums) {
+            if (n % 2 == 0) {
+                freq[n]++;
+            }
+        }
+        if (freq.empty()) {
+            return -1;
+        }
+        
+        int minVal = INT_MAX;
+        int maxFreq = INT_MIN;
+        for (auto [val, f] : freq) {
+            if (f > maxFreq) {
+                minVal = val;
+                maxFreq = f;
+            } else if (val < minVal && f >= maxFreq) {
+                minVal = val;
+                maxFreq = f;
+            }
+        }
+        cout << endl;
+        return minVal;
+    }
+};
