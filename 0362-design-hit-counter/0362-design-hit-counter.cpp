f963f73bb1bc9d0f2412c3d8ec9e5ec5d1a38efa
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2024-05-05 08:56:46 -0700

    Time: 0 ms (100.00%), Space: 9.1 MB (39.09%) - LeetHub

---
+++ 0362-design-hit-counter/0362-design-hit-counter.cpp
@@ -0,0 +1,26 @@
+class HitCounter {
+    queue<int> q;
+    
+public:
+    HitCounter() {
+        
+    }
+    
+    void hit(int timestamp) {
+        q.push(timestamp);
+    }
+    
+    int getHits(int timestamp) {
+        while (!q.empty() && q.front() <= timestamp - 300) {
+            q.pop();
+        }
+        return q.size();
+    }
+};
+
+/**
+ * Your HitCounter object will be instantiated and called as such:
+ * HitCounter* obj = new HitCounter();
+ * obj->hit(timestamp);
+ * int param_2 = obj->getHits(timestamp);
+ */
