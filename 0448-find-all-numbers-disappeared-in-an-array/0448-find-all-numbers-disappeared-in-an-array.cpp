e13b0bae22b0c202f3ca6bf00d05522c88fa621a
ken-matsui <ken-matsui@users.noreply.github.com>
2024-05-06 11:55:47 +0000

    Apply automatic changes

--- 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.cpp
+++ 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     vector<int> findDisappearedNumbers(vector<int>& nums) {
         unordered_set<int> s(nums.begin(), nums.end());
-        
+
         vector<int> res;
         for (int i = 1; i <= nums.size(); ++i) {
             if (!s.contains(i)) {
