fde6ba6792871d05f2fbed55b925924e97448158
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2024-05-06 06:36:07 -0700

    Time: 0 ms (100.00%), Space: 8.5 MB (25.00%) - LeetHub

---
+++ 3136-valid-word/3136-valid-word.cpp
@@ -0,0 +1,35 @@
+class Solution {
+    unordered_set<char> vowels{
+        'a', 'e', 'i', 'o', 'u'
+    };
+
+public:
+    bool isValid(string word) {
+        if (word.size() < 3) {
+            return false;
+        }
+
+        for (char c : word) {
+            if (!isalnum(c)) {
+                return false;
+            }
+        }
+        
+        bool hasVowel = false;
+        bool hasConso = false;
+        for (char c : word) {
+            if (isalpha(c)) {
+                if (vowels.contains(tolower(c))) {
+                    hasVowel = true;
+                } else {
+                    hasConso = true;
+                }
+
+                if (hasVowel && hasConso) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+};
