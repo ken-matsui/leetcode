8878fa2f8c4d9536a485a2e7d1b9d596c1f3ab92
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2024-05-05 23:18:58 -0700

    Time: 0 ms (100.00%), Space: 8.1 MB (28.96%) - LeetHub

---
+++ 1507-reformat-date/1507-reformat-date.cpp
@@ -0,0 +1,31 @@
+class Solution {
+    unordered_map<string, string> month{
+        {"Jan", "01"},
+        {"Feb", "02"},
+        {"Mar", "03"},
+        {"Apr", "04"},
+        {"May", "05"},
+        {"Jun", "06"},
+        {"Jul", "07"},
+        {"Aug", "08"},
+        {"Sep", "09"},
+        {"Oct", "10"},
+        {"Nov", "11"},
+        {"Dec", "12"},
+    };
+    
+public:
+    string reformatDate(string date) {
+        string res = date.substr(date.size() - 4, 4);
+        res += "-";
+        res += month[date.substr(date.size() - 8, 3)];
+        res += "-";
+        if (!isdigit(date[1])) {
+            res += "0";
+            res += date[0];
+        } else {
+            res += date.substr(0, 2);
+        }
+        return res;
+    }
+};
