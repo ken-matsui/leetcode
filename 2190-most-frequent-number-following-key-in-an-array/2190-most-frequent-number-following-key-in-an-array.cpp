2ac9dbcfb720f17c0ad51dc79b31564047a2ae28
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2024-05-06 03:28:30 -0700

    Time: 6 ms (76.29%), Space: 15.4 MB (76.60%) - LeetHub

---
+++ 2190-most-frequent-number-following-key-in-an-array/2190-most-frequent-number-following-key-in-an-array.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int mostFrequent(vector<int>& nums, int key) {
+        unordered_map<int, int> m;
+        for (int i = 0; i < nums.size() - 1; ++i) {
+            if (nums[i] == key) {
+                m[nums[i + 1]]++;
+            }
+        }
+        
+        int maxFreq = INT_MIN;
+        int maxVal = INT_MIN;
+        for (const auto& [target, count] : m) {
+            if (count > maxFreq) {
+                maxFreq = count;
+                maxVal = target;
+            }
+        }
+        return maxVal;
+    }
+};
