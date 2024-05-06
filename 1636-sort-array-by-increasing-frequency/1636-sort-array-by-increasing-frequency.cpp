2b66bb95cc373b03d4f25407d6e0f45e44df95f4
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2024-05-06 00:28:08 -0700

    Time: 0 ms (100.00%), Space: 15 MB (10.97%) - LeetHub

---
+++ 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<int> frequencySort(vector<int>& nums) {
+        unordered_map<int, int> freq;
+        for (int n : nums) {
+            freq[n]++;
+        }
+        
+        auto cmp = [](const auto& lhs, const auto& rhs) {
+            if (lhs.first != rhs.first) {
+                return lhs.first > rhs.first;
+            }
+            return lhs.second < rhs.second;
+        };
+        priority_queue<pair<int, int>, vector<pair<int, int>>, decltype(cmp)> pq(cmp);
+        for (auto [val, f] : freq) {
+            pq.emplace(f, val);
+        }
+
+        vector<int> res(nums.size());
+        for (int i = 0; i < nums.size() && !pq.empty();) {
+            auto [f, val] = pq.top();
+            pq.pop();
+            
+            for (int j = 0; j < f; ++j) {
+                res[i + j] = val;
+            }
+            i += f;
+        }
+        return res;
+    }
+};
