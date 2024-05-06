d01f0814f248163de460dbbac5eec8285862ec5c
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2024-05-06 04:11:48 -0700

    Time: 10 ms (38.12%), Space: 14.1 MB (87.29%) - LeetHub

---
+++ 1213-intersection-of-three-sorted-arrays/1213-intersection-of-three-sorted-arrays.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<int> arraysIntersection(vector<int>& arr1, vector<int>& arr2, vector<int>& arr3) {
+        vector<int> res;
+        for (int i = 0; i < arr1.size(); ++i) {
+            if (binary_search(arr2.begin(), arr2.end(), arr1[i]) && binary_search(arr3.begin(), arr3.end(), arr1[i])) {
+                res.push_back(arr1[i]);
+            }
+        }
+        return res;
+    }
+};
