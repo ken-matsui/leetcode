cb200bff75cef22c52bf749c223eb3249c694513
ken-matsui <ken-matsui@users.noreply.github.com>
2024-05-06 10:37:28 +0000

    Apply automatic changes

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
@@ -3,9 +3,10 @@ public:
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
         set<int> nums1_set(nums1.begin(), nums1.end());
         set<int> nums2_set(nums2.begin(), nums2.end());
-        
+
         vector<int> res;
-        set_intersection(nums1_set.begin(), nums1_set.end(), nums2_set.begin(), nums2_set.end(), back_inserter(res));
+        set_intersection(nums1_set.begin(), nums1_set.end(), nums2_set.begin(),
+                         nums2_set.end(), back_inserter(res));
         return res;
     }
 };
