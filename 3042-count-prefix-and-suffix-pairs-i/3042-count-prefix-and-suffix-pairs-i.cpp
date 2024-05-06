1abd65b27e6b5d499ad14b0ee0c4873dd6945762
ken-matsui <ken-matsui@users.noreply.github.com>
2024-05-06 06:24:51 +0000

    Apply automatic changes

--- 3042-count-prefix-and-suffix-pairs-i/3042-count-prefix-and-suffix-pairs-i.cpp
+++ 3042-count-prefix-and-suffix-pairs-i/3042-count-prefix-and-suffix-pairs-i.cpp
@@ -4,7 +4,8 @@ public:
         int count = 0;
         for (int i = 0; i < words.size(); ++i) {
             for (int j = i + 1; j < words.size(); ++j) {
-                if (words[j].starts_with(words[i]) && words[j].ends_with(words[i])) {
+                if (words[j].starts_with(words[i]) &&
+                    words[j].ends_with(words[i])) {
                     ++count;
                 }
             }
