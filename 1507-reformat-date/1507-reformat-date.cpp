26bc303d1e65888d6e08a6bddedea27c39c5f081
ken-matsui <ken-matsui@users.noreply.github.com>
2024-05-06 06:20:51 +0000

    Apply automatic changes

--- 1507-reformat-date/1507-reformat-date.cpp
+++ 1507-reformat-date/1507-reformat-date.cpp
@@ -1,19 +1,10 @@
 class Solution {
     unordered_map<string, string> month{
-        {"Jan", "01"},
-        {"Feb", "02"},
-        {"Mar", "03"},
-        {"Apr", "04"},
-        {"May", "05"},
-        {"Jun", "06"},
-        {"Jul", "07"},
-        {"Aug", "08"},
-        {"Sep", "09"},
-        {"Oct", "10"},
-        {"Nov", "11"},
-        {"Dec", "12"},
+        {"Jan", "01"}, {"Feb", "02"}, {"Mar", "03"}, {"Apr", "04"},
+        {"May", "05"}, {"Jun", "06"}, {"Jul", "07"}, {"Aug", "08"},
+        {"Sep", "09"}, {"Oct", "10"}, {"Nov", "11"}, {"Dec", "12"},
     };
-    
+
 public:
     string reformatDate(string date) {
         string res = date.substr(date.size() - 4, 4);
