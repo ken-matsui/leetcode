8afc6fae0bf7fa788bd9c67b2d03fe714acfe2fc
ken-matsui <ken-matsui@users.noreply.github.com>
2024-05-07 18:31:32 +0000

    Apply automatic changes

--- 0243-shortest-word-distance/0243-shortest-word-distance.cpp
+++ 0243-shortest-word-distance/0243-shortest-word-distance.cpp
@@ -1,11 +1,12 @@
 class Solution {
 public:
-    int shortestDistance(vector<string>& wordsDict, string word1, string word2) {
+    int shortestDistance(vector<string>& wordsDict, string word1,
+                         string word2) {
         unordered_map<string, vector<int>> words;
         for (int i = 0; i < wordsDict.size(); ++i) {
             words[wordsDict[i]].push_back(i);
         }
-        
+
         int minDist = INT_MAX;
         for (int i : words[word1]) {
             for (int j : words[word2]) {
