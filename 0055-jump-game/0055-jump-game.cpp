fe2c16b21747dab58c4fdff1bd2050dc4a01b101
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2024-06-08 01:40:25 -0700

    Time: 44 ms (60.69%), Space: 50.8 MB (81.22%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        for (int i = 0; i < nums.size() - 1; ++i) {
+            if (nums[i] != 0) {
+                continue;
+            }
+            
+            // found 0; check if we can skip this 0
+            bool canSkip = false;
+            int reqToSkip = 1;
+            for (int j = i - 1; j >= 0; --j) {
+                if (nums[j] > reqToSkip) {
+                    // can skip this 0
+                    canSkip = true;
+                }
+                ++reqToSkip;
+            }
+            if (!canSkip) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
