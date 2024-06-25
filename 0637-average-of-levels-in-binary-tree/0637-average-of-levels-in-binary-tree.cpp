b8f4e69c8416ef57b866397dfd063e25172b73da
ken-matsui <ken-matsui@users.noreply.github.com>
2024-06-25 17:39:16 +0000

    Apply automatic changes

--- 0637-average-of-levels-in-binary-tree/0637-average-of-levels-in-binary-tree.cpp
+++ 0637-average-of-levels-in-binary-tree/0637-average-of-levels-in-binary-tree.cpp
@@ -6,21 +6,22 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
     vector<vector<int>> values;
-    
+
     void rec(TreeNode* node, int level) {
         if (!node) {
             return;
         }
-        
+
         if (values.size() <= level) {
             values.push_back(vector<int>{});
         }
-        
+
         values[level].push_back(node->val);
         rec(node->left, level + 1);
         rec(node->right, level + 1);
@@ -31,7 +32,7 @@ public:
         if (!root) {
             return {};
         }
-        
+
         rec(root, 0);
         vector<double> result;
         for (const auto& vec : values) {
